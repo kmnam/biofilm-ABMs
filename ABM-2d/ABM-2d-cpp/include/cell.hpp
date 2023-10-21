7c075cf74e9e5ccc0baacea688026a76249a2d48
Kee Myoung Nam <kn449@login2.mccleary.ycrc.yale.edu>
2023-10-21 14:09:53 -0400

    First commits

---
+++ ABM-2d/ABM-2d-cpp/include/cell.hpp
@@ -0,0 +1,23 @@
+/**
+ * A simple class for spherocylindrical cells. 
+ *
+ * Authors:
+ *     Kee-Myoung Nam
+ *
+ * Last updated:
+ *     10/21/2023
+ */
+
+#ifndef BIOFILM_CELL_HPP
+#define BIOFILM_CELL_HPP
+
+/**
+ * A simple containiner class for spherocylindrical cells with a rigid 
+ * inner body and an elastic outer coating. 
+ */
+template <typename T>
+class Cell
+{
+};
+
+#endif
---
+++ ABM-2d/ABM-2d-cpp/include/mechanics.hpp
@@ -0,0 +1,14 @@
+/**
+ * Implementations of cell-cell and cell-surface interaction forces.
+ *
+ * Authors:
+ *     Kee-Myoung Nam
+ *
+ * Last updated:
+ *     10/21/2023
+ */
+
+#ifndef BIOFILM_MECHANICS_HPP
+#define BIOFILM_MECHANICS_HPP
+
+#endif
