20be7ce1cbad41a97fab81b4af55fdec8fb20763
Kee Myoung Nam <kmnam.99.12@gmail.com>
2023-10-22 20:09:46 -0400

    First implementation of von Mises sampling

--- ABM-2d/ABM-2d-cpp/include/utils.hpp
+++ ABM-2d/ABM-2d-cpp/include/utils.hpp
@@ -10,22 +10,23 @@
 
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <cmath>
 #include <boost/json/src.hpp>
-
-using namespace boost::json; 
+#include <boost/math/constants/constants.hpp>
 
 /**
- * Parse a JSON file specifying ABM configurations.
+ * Parse a JSON file specifying simulation parameters.
  *
  * @param filename Input JSON configurations file.
  * @returns `boost::json::value` instance containing the JSON data.  
  */
-value parseConfigFile(const std::string filename)
+boost::json::value parseConfigFile(const std::string filename)
 {
     std::string line;
     std::ifstream infile(filename);
-    stream_parser p; 
-    error_code ec;  
+    boost::json::stream_parser p; 
+    boost::json::error_code ec;  
     while (std::getline(infile, line))
     {
         p.write(line, ec); 
@@ -37,5 +38,88 @@ value parseConfigFile(const std::string filename)
         return nullptr;
     
     return p.release(); 
+}
+
+/**
+ * Write a population of cells, with the corresponding simulation parameters,
+ * to the given path. 
+ *
+ * @param cells Existing population of cells.
+ * @param params `boost::json::object` instance containing the JSON data. 
+ * @param filename Output file. 
+ */
+template <typename T>
+void writeCells(const Ref<const Array<T, Dynamic, Dynamic> >& cells, 
+                boost::json::object& params, const std::string filename)
+{
+    // Open output file 
+    std::ofstream outfile(filename);
+    
+    // Run through the simulation parameters ... 
+    for (auto&& param : params)
+    {
+        std::string key = param.key().to_string(); 
+        outfile << "# " << key << " = " << params.at(key) << std::endl;
+    }
+
+    // Write each cell in the population ...
+    outfile << std::setprecision(10);  
+    for (int i = 0; i < cells.rows(); ++i)
+    {
+        for (int j = 0; j < cells.cols(); ++j)
+        {
+            if (j < 9)         // If the entry is a floating-point value
+                outfile << cells(i, j) << '\t'; 
+            else if (j == 9)   // If the entry is a group identifier 
+                outfile << static_cast<int>(cells(i, j)) << '\t'; 
+        }
+        outfile.seekp(-1, std::ios_base::cur);
+        outfile << std::endl;
+    }
+
+    // Close output file 
+    outfile.close();  
+}
+
+/**
+ * Sample a value from the von Mises distribution with the given mean and
+ * concentration parameter.
+ *
+ * This is an implementation of Best & Fisher's algorithm. 
+ *
+ * @param mu Mean. 
+ * @param kappa Concentration parameter.
+ * @param rng Random number generator.
+ * @param uniform_dist Pre-defined instance of standard uniform distribution.
+ * @returns A sampled value from the von Mises distribution.
+ */
+template <typename T>
+T vonMises(const T mu, const T kappa, boost::random::mt19937& rng,
+           boost::random::uniform_01<>& uniform_dist)
+{
+    T tau = 1 + std::sqrt(1 + 4 * kappa * kappa);
+    T rho = (tau - std::sqrt(2 * tau)) / (2 * kappa);
+    T r = (1 + rho * rho) / (2 * rho); 
+    bool reject = true; 
+    while (reject)
+    {
+        T u1 = uniform_dist(rng); 
+        T u2 = uniform_dist(rng);
+        T u3 = uniform_dist(rng); 
+        T z = std::cos(boost::math::constants::pi<T>() * u1);
+        T f = (1 + r * z) / (r + z); 
+        T c = kappa * (r - f); 
+        if (c * (2 - c) - u2 > 0)
+            reject = false; 
+        else if (std::log(c / u2) + 1 - c >= 0)
+            reject = false;
+    }
+
+    if (u3 - 0.5 > 0)
+        return (std::acos(f) + mu) % boost::math::constants::two_pi<T>();
+    else (u3 - 0.5 < 0)
+        return (-std::acos(f) + mu) % boost::math::constants::two_pi<T>(); 
+    else 
+        return mu;
 }
 
