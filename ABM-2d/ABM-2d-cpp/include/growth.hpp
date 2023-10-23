c43c59ef6de67d23e7bd15e097beb7fffb14ab16
Kee Myoung Nam <kmnam.99.12@gmail.com>
2023-10-22 20:37:31 -0400

    Implemented new version of divideCells()

--- ABM-2d/ABM-2d-cpp/include/growth.hpp
+++ ABM-2d/ABM-2d-cpp/include/growth.hpp
@@ -223,3 +223,154 @@ Array<T, Dynamic, Dynamic> divideCells(const Ref<const Array<T, Dynamic, Dynamic
         return cells; 
     }
 } 
+
+/**
+ * Divide the indicated cells at the given time.
+ *
+ * This is an extended version of `divideCells()` that assigns group-specific
+ * growth rates. The groups are assumed to be numbered 1, 2, 3, ...
+ *
+ * @param cells Existing population of cells.
+ * @param t Current time.
+ * @param R Cell radius. 
+ * @param to_divide Boolean index indicating which cells are to divide.
+ * @param growth_dists Vector of function instances specifying the growth
+ *                     rate distribution for each group. Each function must
+ *                     take boost::random::mt19937& as its single argument.
+ * @param rng Random number generator. 
+ * @param daughter_length_dist Function instance specifying the daughter 
+ *                             cell length ratio distribution. 
+ * @param daughter_angle_dist Function instance specifying the daughter 
+ *                            cell orientation distribution.
+ * @returns Updated population of cells. 
+ */
+template <typename T>
+Array<T, Dynamic, Dynamic> divideCells(const Ref<const Array<T, Dynamic, Dynamic> >& cells,
+                                       const T t, const T R,
+                                       const Ref<const Array<int, Dynamic, 1> >& to_divide,
+                                       std::vector<std::function<T(boost::random::mt19937&)> >& growth_dists,
+                                       boost::random::mt19937& rng,
+                                       std::function<T(boost::random::mt19937&)>& daughter_length_dist,
+                                       std::function<T(boost::random::mt19937&)>& daughter_angle_dist)
+{
+    // If there are cells to be divided ...
+    const int n_divide = to_divide.sum();
+    if (n_divide > 0)
+    {
+        // Get indices of cells to be divided
+        int n = cells.rows(); 
+        std::vector<int> idx_divide;
+        for (int i = 0; i < n; ++i)
+        {
+            if (to_divide(i))
+                idx_divide.push_back(i);
+        }
+
+        // Get an extended copy of the existing population
+        Array<T, Dynamic, Dynamic> cells_total(n + n_divide, cells.cols()); 
+        cells_total(Eigen::seqN(0, n), Eigen::all) = cells; 
+
+        // Get a copy of the corresponding sub-array
+        Array<T, Dynamic, Dynamic> new_cells(cells(idx_divide, Eigen::all)); 
+
+        // Update cell orientations ...
+        //
+        // Each daughter cell has an orientation that is obtained by 
+        // rotating the dividing cell's orientation counterclockwise by
+        // a angle theta, which is sampled using daughter_angle_dist()
+        Array<T, Dynamic, 2> dividing_orientations(new_cells(Eigen::all, Eigen::seq(2, 3))); 
+        Array<T, Dynamic, 1> theta1(n_divide), theta2(n_divide); 
+        for (int i = 0; i < n_divide; ++i)
+        {
+            theta1(i) = daughter_angle_dist(rng); 
+            theta2(i) = daughter_angle_dist(rng);
+        }
+        Array<T, Dynamic, 1> cos_theta1 = theta1.cos(); 
+        Array<T, Dynamic, 1> sin_theta1 = theta1.sin(); 
+        Array<T, Dynamic, 1> cos_theta2 = theta2.cos(); 
+        Array<T, Dynamic, 1> sin_theta2 = theta2.sin(); 
+        for (int i = 0; i < n_divide; ++i)
+        {
+            Matrix<T, 2, 2> rot(2, 2); 
+            rot << cos_theta1(i), -sin_theta1(i),
+                   sin_theta1(i),  cos_theta1(i); 
+            cells_total(idx_divide[i], Eigen::seq(2, 3)) =
+                (rot * dividing_orientations.matrix().row(i).transpose()).array();
+            rot << cos_theta2(i), -sin_theta2(i),
+                   sin_theta2(i),  cos_theta2(i); 
+            new_cells(i, Eigen::seq(2, 3)) =
+                (rot * dividing_orientations.matrix().row(i).transpose()).array();  
+        }
+
+        // Update cell lengths and positions ... 
+        //
+        // If cell division is symmetric, then the daughter cells both have
+        // length L / 2 - R, where L is the length of the dividing cell, 
+        // the point of division occurs at the center of the dividing cell,
+        // and their x- and y-coordinates are perturbed by L / 4 + R / 2
+        // along the daughter cells' orientation vectors 
+        //
+        // If not, then the daughter cells have lengths L1 = M * (L - 2 * R)
+        // and L2 = (1 - M) * (L - 2 * R), where M is a random variable between
+        // 0 and 1. The point of division occurs at cell body coordinate
+        // L1 + R - Lh with Lh = L / 2, which has x- and y-coordinates given
+        // by rx + (L1 + R - Lh) * nx and ry + (L1 + R - Lh) * ny, where
+        // (rx, ry) and (nx, ny) are the position and orientation of the
+        // dividing cell. The corresponding perturbations of the cell centers
+        // from the point of division are given by R + M * (L - 2 * R) / 2 and
+        // R + (1 - M) * (L - 2 * R) / 2
+        //
+        // Sample a normally distributed daughter cell length for each cell
+        // to be divided 
+        Array<T, Dynamic, 1> M(n_divide); 
+        for (int i = 0; i < n_divide; ++i)
+            M(i) = daughter_length_dist(rng);
+        // Locate point of division along dividing cell centerline 
+        Array<T, Dynamic, 1> div = (
+            M * (cells_total(idx_divide, 4) - 2 * R) - (cells_total(idx_divide, 4)) / 2 + R
+        );
+        // Get perturbations from point of division along cell centerline
+        // for the daughter cell centers
+        Array<T, Dynamic, 1> delta1 = M * (cells_total(idx_divide, 4) - 2 * R) / 2 + R; 
+        Array<T, Dynamic, 1> delta2 = (1 - M) * (new_cells.col(4) - 2 * R) / 2 + R;
+        // Define daughter cell lengths and locate daughter cell centers
+        cells_total(idx_divide, 4) = M * (cells_total(idx_divide, 4) - 2 * R); 
+        new_cells.col(4) = (1 - M) * (new_cells.col(4) - 2 * R); 
+        cells_total(idx_divide, 0) = (
+            cells_total(idx_divide, 0) + (div - delta1) * cells_total(idx_divide, 2)
+        );
+        new_cells.col(0) = new_cells.col(0) + (div + delta2) * new_cells.col(2);
+        cells_total(idx_divide, 1) = (
+            cells_total(idx_divide, 1) + (div - delta1) * cells_total(idx_divide, 3)
+        );
+        new_cells.col(1) = new_cells.col(1) + (div + delta2) * new_cells.col(3);
+
+        // Update cell birth times
+        cells_total(idx_divide, 5) = t;
+        new_cells.col(5) = t;
+
+        // Update cell growth rates (sample from specified distributions)
+        for (auto it = idx_divide.begin(); it != idx_divide.end(); ++it)
+        {
+            int group = static_cast<int>(cells_total(*it, 9)); 
+            cells_total(*it, 6) = growth_dists[group - 1](rng);
+        } 
+        for (int i = 0; i < n_divide; ++i)
+        {
+            int group = static_cast<int>(new_cells(i, 9));
+            new_cells(i, 6) = growth_dists[group - 1](rng);
+        }
+
+        // Copy over daughter cell data
+        //
+        // Note that each daughter cell inherits its mother cell's viscosity
+        // and friction coefficient
+        cells_total(Eigen::seqN(n, n_divide), Eigen::all) = new_cells;  
+        
+        return cells_total; 
+    }
+    else 
+    {
+        return cells; 
+    }
+} 
