ab1d02649661b3fe6b3874bf70279efc5e3da65e
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-01-21 12:53:18 -0600

    Updated integral calculations

--- ABM-3d/ABM-3d-cpp/include/integrals.hpp
+++ ABM-3d/ABM-3d-cpp/include/integrals.hpp
@@ -6,7 +6,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     1/8/2023
+ *     1/20/2023
  */
 
 #ifndef BIOFILM_3D_AUXILIARY_INTEGRALS_HPP
@@ -17,212 +17,298 @@
 #include <boost/math/constants/constants.hpp>
 
 /**
- * Compute the integral of a cell-surface overlap, \delta_i(s), raised by
- * an exponent, \gamma, from s = -l_i/2 to s = +l_i/2, where l_i is the 
- * length of the cell.
+ * Return the cell-body coordinate at which the cell-surface overlap is zero.
  *
  * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
- * @param R Cell radius. 
- * @param gamma Exponent; should not be -1.
- * @returns Desired integral.  
+ * @param R Cell radius.
+ * @returns Cell-body coordinate at which the cell-surface overlap is zero.
  */
 template <typename T>
-T integral1(T rz, T nz, T half_l, T R, T gamma)
+T sstar(const T rz, const T nz, const T R)
 {
-    T overlap1 = std::pow(R - rz + half_l * nz, gamma + 1); 
-    T overlap2 = std::pow(R - rz - half_l * nz, gamma + 1);
-    return -(overlap2 - overlap1) / ((gamma + 1) * nz); 
+    return (R - rz) / nz;
 }
 
 /**
- * Compute the integral of s * \delta_i^\gamma(s), where \delta_i(s) is 
- * the cell-surface overlap and \gamma is a real exponent, from s = -l_i/2
- * to s = +l_i/2, where l_i is the length of the cell.
- *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * Return the cell-surface overlap, \phi_i(s).
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
- * @param R Cell radius. 
- * @param gamma Exponent; should not be -1 or -2.
- * @returns Desired integral.  
+ * @param R Cell radius.
+ * @param s Cell-body coordinate. 
+ * @returns Cell-surface overlap. 
  */
 template <typename T>
-T integral2(T rz, T nz, T half_l, T R, T gamma)
+T phi(const T rz, const T nz, const T R, const T s)
 {
-    T overlap1 = std::pow(R - rz + half_l * nz, gamma + 1); 
-    T overlap2 = std::pow(R - rz - half_l * nz, gamma + 1);
-    T term1 = -half_l * (overlap2 + overlap1);
-    T term2 = integral1(rz, nz, half_l, R, gamma + 1);
-    return (term1 + term2) / ((gamma + 1) * nz); 
+    return R - rz - s * nz;
 }
 
 /**
- * Compute the integral of s^2 * \delta_i^\gamma(s), where \delta_i(s) is 
- * the cell-surface overlap and \gamma is a real exponent, from s = -l_i/2
- * to s = +l_i/2, where l_i is the length of the cell.
- *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * Return the cell-surface overlap, \delta_i(s).
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
- * @param R Cell radius. 
- * @param gamma Exponent; should not be -1 or -2 or -3.
- * @returns Desired integral.  
+ * @param R Cell radius.
+ * @param s Cell-body coordinate. 
+ * @returns Cell-surface overlap. 
  */
 template <typename T>
-T integral3(T rz, T nz, T half_l, T R, T gamma)
+T overlap(const T rz, const T nz, const T R, const T s)
 {
-    T overlap1 = std::pow(R - rz + half_l * nz, gamma + 1); 
-    T overlap2 = std::pow(R - rz - half_l * nz, gamma + 1);
-    T term1 = -half_l * half_l * (overlap2 - overlap1);
-    T term2 = 2 * integral2(rz, nz, half_l, R, gamma + 1);
-    return (term1 + term2) / ((gamma + 1) * nz); 
-}
-
-/**
- * The Heaviside step function.
- *
- * @param s Input value.
- * @returns The Heaviside step function evaluated at s, i.e., +1 if s >= 0,
- *          0 otherwise. 
- */
-template <typename T>
-T step(T s)
-{
-    if (s >= 0)
-        return 1;
+    T p = phi(rz, nz, R, s);
+    if (p > 0)
+        return p;
     else
         return 0;
 }
 
 /**
- * Compute the integral of \Theta(\delta_i(s)), where \Theta is the Heaviside
- * step function and \delta_i(s) is the cell-surface overlap, from s = -l_i/2
- * to s = +l_i/2, where l_i is the length of the cell.
- *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * Return the exponentiated cell-surface overlap, \delta_i^\gamma(s), where 
+ * \gamma is a real exponent.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param s Cell-body coordinate. 
+ * @param gamma Exponent.
+ * @returns Exponentiated cell-surface overlap. 
+ */
+template <typename T>
+T overlapGamma(const T rz, const T nz, const T R, const T s, const T gamma)
+{
+    T p = phi(rz, nz, R, s); 
+    if (p > 0)
+        return std::pow(p, gamma);
+    else
+        return 0;
+}
+
+/**
+ * Compute the integral of \delta_i^\gamma(s), where \gamma is a real exponent,
+ * from s = -l_i/2 to s = +l_i/2, where l_i is the length of the cell.
+ *
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
+ *
+ * @param rz z-coordinate of cell center.
+ * @param nz z-coordinate of cell orientation.
+ * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param gamma Exponent; should not be -1.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.  
  */
 template <typename T>
-T integral4(T rz, T nz, T half_l, T R)
+T integral1(const T rz, const T nz, const T R, const T half_l, const T gamma,
+            const T ss)
 {
-    T overlap1 = R - rz + half_l * nz; 
-    T overlap2 = R - rz - half_l * nz;
-    T step1 = step(overlap1);
-    T step2 = step(overlap2);
-    return -(step2 * overlap2 - step1 * overlap1) / nz;
+    if (ss < -half_l)
+    {
+        T overlap1 = std::pow(phi(rz, nz, R, -half_l), gamma + 1);
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        return -(overlap2 - overlap1) / (nz * (gamma + 1));
+    }
+    else if (ss < half_l)
+    {
+        // overlap1 = 0
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        return -overlap2 / (nz * (gamma + 1));
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+/**
+ * Compute the integral of s * \delta_i^\gamma(s), where \gamma is a real
+ * exponent, from s = -l_i/2 to s = +l_i/2, where l_i is the length of the
+ * cell.
+ *
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
+ *
+ * @param rz z-coordinate of cell center.
+ * @param nz z-coordinate of cell orientation.
+ * @param R Cell radius. 
+ * @param half_l Half of cell length.
+ * @param gamma Exponent; should not be -1 or -2.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
+ * @returns Desired integral.  
+ */
+template <typename T>
+T integral2(const T rz, const T nz, const T R, const T half_l, const T gamma,
+            const T ss)
+{
+    T term1 = integral1(rz, nz, R, half_l, gamma + 1, ss);
+    T term2 = 0; 
+    if (ss < -half_l)
+    {
+        T overlap1 = std::pow(phi(rz, nz, R, -half_l), gamma + 1);
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        term2 = half_l * overlap2 + half_l * overlap1;    // = ... - (-half_l) * overlap1
+    }
+    else if (ss < half_l)
+    {
+        // Overlap at s = ss is zero 
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        term2 = half_l * overlap2;
+    }
+    return (term1 - term2) / (nz * (gamma + 1)); 
+}
+
+/**
+ * Compute the integral of s^2 * \delta_i^\gamma(s), where \gamma is a real
+ * exponent, from s = -l_i/2 to s = +l_i/2, where l_i is the length of the
+ * cell.
+ *
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
+ *
+ * @param rz z-coordinate of cell center.
+ * @param nz z-coordinate of cell orientation.
+ * @param R Cell radius. 
+ * @param half_l Half of cell length.
+ * @param gamma Exponent; should not be -1 or -2.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
+ * @returns Desired integral.  
+ */
+template <typename T>
+T integral3(const T rz, const T nz, const T R, const T half_l, const T gamma,
+            const T ss)
+{
+    T term1 = 2 * integral2(rz, nz, R, half_l, gamma + 1, ss);
+    T term2 = 0;
+    if (ss < -half_l)
+    {
+        T overlap1 = std::pow(phi(rz, nz, R, -half_l), gamma + 1);
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        term2 = half_l * half_l * (overlap2 - overlap1);
+    }
+    else if (ss < half_l)
+    {
+        // Overlap at s = ss is zero 
+        T overlap2 = std::pow(phi(rz, nz, R, half_l), gamma + 1);
+        term2 = half_l * half_l * overlap2;
+    }
+    return (term1 - term2) / (nz * (gamma + 1));
+}
+
+/**
+ * Compute the integral of \Theta(\delta_i(s)), where \Theta is the Heaviside
+ * step function, from s = -l_i/2 to s = +l_i/2, where l_i is the length of
+ * the cell.
+ *
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
+ *
+ * TODO Fix arguments here
+ *
+ * @param rz z-coordinate of cell center.
+ * @param nz z-coordinate of cell orientation.
+ * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
+ * @returns Desired integral.  
+ */
+template <typename T>
+T integral4(const T rz, const T nz, const T R, const T half_l, const T ss)
+{
+    if (ss < -half_l)
+        return 2 * half_l;
+    else if (ss < half_l)
+        return half_l - ss;
+    else
+        return 0;
 }
 
 /**
  * Compute the integral of s * \Theta(\delta_i(s)), where \Theta is the
- * Heaviside step function and \delta_i(s) is the cell-surface overlap, 
- * from s = -l_i/2 to s = +l_i/2, where l_i is the length of the cell.
+ * Heaviside step function, from s = -l_i/2 to s = +l_i/2, where l_i is the
+ * length of the cell.
  *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
- *
- * @param rz z-coordinate of cell center.
- * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
- * @param R Cell radius.
- * @returns Desired integral.  
- */
-template <typename T>
-T integral5(T rz, T nz, T half_l, T R)
-{
-    T overlap1 = R - rz + half_l * nz;
-    T overlap2 = R - rz - half_l * nz;
-    T step1 = step(overlap1);
-    T step2 = step(overlap2);
-    T term1 = -half_l * (step2 * overlap2 + step1 * overlap1) / nz;
-    T term2 = (-step2 * overlap2 * overlap2 + step1 * overlap1 * overlap1) / (2 * nz * nz);
-    return term1 + term2; 
-}
-
-/**
- * Compute the integral of s * \delta_i(s) * \Theta(\delta_i(s)), where
- * \Theta is the Heaviside step function and \delta_i(s) is the cell-surface
- * overlap, from s = -l_i/2 to s = +l_i/2, where l_i is the length of the cell.
- *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.  
  */
 template <typename T>
-T integral6(T rz, T nz, T half_l, T R)
+T integral5(const T rz, const T nz, const T R, const T half_l, const T ss)
 {
-    T overlap1 = R - rz + half_l * nz;
-    T overlap2 = R - rz - half_l * nz;
-    T step1 = step(overlap1);
-    T step2 = step(overlap2);
-    T term1 = -half_l * (step2 * overlap2 * overlap2 + step1 * overlap1 * overlap1) / (2 * nz);
-    T term2 = (-step2 * overlap2 * overlap2 * overlap2 + step1 * overlap1 * overlap1 * overlap1) / (6 * nz * nz);
-    return term1 + term2; 
+    T term1 = integral1(rz, nz, R, half_l, 1.0, ss);
+    T term2 = 0;
+    if (ss < -half_l)
+    {
+        T overlap1 = phi(rz, nz, R, -half_l);
+        T overlap2 = phi(rz, nz, R, half_l);
+        term2 = half_l * overlap2 + half_l * overlap1;   // = ... - (-half_l) * overlap1
+    }
+    else if (ss < half_l)
+    {
+        // Overlap at s = ss is zero 
+        T overlap2 = phi(rz, nz, R, half_l);
+        term2 = half_l * overlap2;
+    }
+    return (term1 - term2) / nz;
 }
 
 /**
  * Compute the integral of s^2 * \Theta(\delta_i(s)), where \Theta is the
- * Heaviside step function and \delta_i(s) is the cell-surface overlap, from
- * s = -l_i/2 to s = +l_i/2, where l_i is the length of the cell.
+ * Heaviside step function, from s = -l_i/2 to s = +l_i/2, where l_i is the
+ * length of the cell.
  *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.  
  */
 template <typename T>
-T integral7(T rz, T nz, T half_l, T R)
+T integral6(const T rz, const T nz, const T R, const T half_l, const T ss)
 {
-    T overlap1 = R - rz + half_l * nz;
-    T overlap2 = R - rz - half_l * nz;
-    T step1 = step(overlap1);
-    T step2 = step(overlap2);
-    T term1 = half_l * half_l * (-step2 * overlap2 + step1 * overlap1) / nz;
-    T term2 = 2 * integral6(rz, nz, half_l, R) / nz;
-    return term1 + term2; 
+    T term1 = 2 * integral2(rz, nz, R, half_l, 1.0, ss);
+    T term2 = 0;
+    if (ss < -half_l)
+    {
+        T overlap1 = phi(rz, nz, R, -half_l);
+        T overlap2 = phi(rz, nz, R, half_l);
+        term2 = half_l * half_l * (overlap2 - overlap1);
+    }
+    else if (ss < half_l)
+    {
+        // Overlap at s = ss is zero 
+        T overlap2 = phi(rz, nz, R, half_l);
+        term2 = half_l * half_l * overlap2;
+    }
+    return (term1 - term2) / nz;
 }
 
 /**
  * Compute the integral of the cell-surface contact area density a_i(s) from
  * s = -l_i/2 to s = +l_i/2, where l_i is the length of the cell.
  *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.
  */
 template <typename T>
-T areaIntegral1(T rz, T nz, T half_l, T R)
+T areaIntegral1(const T rz, const T nz, const T R, const T half_l, const T ss)
 {
-    T overlap1 = R - rz + half_l * nz;
-    T overlap2 = R - rz - half_l * nz;
-    T step1 = step(overlap1);
-    T step2 = step(overlap2);
-    T b = step2 * overlap2 - step1 * overlap1;
-    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * integral1(rz, nz, half_l, R, 0.5);
-    T term2 = boost::math::constants::pi<T>() * R * nz * b;
-    return term1 - term2;
+    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * integral1(rz, nz, R, half_l, 0.5, ss);
+    T term2 = boost::math::constants::pi<T>() * R * nz * nz * integral4(rz, nz, R, half_l, ss);
+    return term1 + term2;
 }
 
 /**
@@ -230,21 +316,20 @@ T areaIntegral1(T rz, T nz, T half_l, T R)
  * contact area density, from s = -l_i/2 to s = +l_i/2, where l_i is the 
  * length of the cell.
  *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.
  */
 template <typename T>
-T areaIntegral2(T rz, T nz, T half_l, T R)
+T areaIntegral2(const T rz, const T nz, const T R, const T half_l, const T ss)
 {
-    T int1 = integral2(rz, nz, half_l, R, 0.5);
-    T int2 = integral5(rz, nz, half_l, R);
-    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * int1;
-    T term2 = boost::math::constants::pi<T>() * R * nz * nz * int2;
+    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * integral2(rz, nz, R, half_l, 0.5, ss);
+    T term2 = boost::math::constants::pi<T>() * R * nz * nz * integral5(rz, nz, R, half_l, ss);
     return term1 + term2;
 }
 
@@ -253,21 +338,20 @@ T areaIntegral2(T rz, T nz, T half_l, T R)
  * contact area density, from s = -l_i/2 to s = +l_i/2, where l_i is the 
  * length of the cell.
  *
- * The z-coordinate of the cell's orientation vector is assumed to be nonzero.
+ * The z-coordinate of the cell's orientation vector is assumed to be negative.
  *
  * @param rz z-coordinate of cell center.
  * @param nz z-coordinate of cell orientation.
- * @param half_l Half of cell length.
  * @param R Cell radius.
+ * @param half_l Half of cell length.
+ * @param ss Pre-computed value of `sstar(rz, nz, R)`.
  * @returns Desired integral.
  */
 template <typename T>
-T areaIntegral3(T rz, T nz, T half_l, T R)
+T areaIntegral3(const T rz, const T nz, const T R, const T half_l, const T ss)
 {
-    T int1 = integral3(rz, nz, half_l, R, 0.5);
-    T int2 = integral7(rz, nz, half_l, R);
-    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * int1;
-    T term2 = boost::math::constants::pi<T>() * R * nz * nz * int2;
+    T term1 = std::pow(R, 0.5) * (1 - nz * nz) * integral3(rz, nz, R, half_l, 0.5, ss);
+    T term2 = boost::math::constants::pi<T>() * R * nz * nz * integral6(rz, nz, R, half_l, ss);
     return term1 + term2;
 }
 
