ba275ae186e3e7a1f05bab233b9b6e7cea10561a
Kee Myoung Nam <kn449@login1.mccleary.ycrc.yale.edu>
2024-01-24 09:56:58 -0500

    Minor edits

--- ABM-3d/ABM-3d-cpp/src/abmDefault.cpp
+++ ABM-3d/ABM-3d-cpp/src/abmDefault.cpp
@@ -128,7 +128,7 @@ int main(int argc, char** argv)
     // mean 0 and given concentration parameter
     boost::random::uniform_01<> uniform_dist;  
     std::function<T(boost::random::mt19937&)> daughter_angle_dist_func =
-        [&orientation_conc, &uniform_dist](boost::random::mt19937& rng)
+        [&orientation_conc, &uniform_dist, &theta_bound](boost::random::mt19937& rng)
         {
             T theta = vonMises<T>(0.0, orientation_conc, rng, uniform_dist);
             while (theta > theta_bound || theta < -theta_bound)
@@ -138,7 +138,7 @@ int main(int argc, char** argv)
 
     // Net force noise distribution: uniform distribution centered at zero
     std::function<T(boost::random::mt19937&)> noise_dist_func =
-        [&uniform_dist](boost::random::mt19937& rng)
+        [&uniform_dist, &max_noise](boost::random::mt19937& rng)
         {
             return -max_noise + 2 * max_noise * uniform_dist(rng);
         };
