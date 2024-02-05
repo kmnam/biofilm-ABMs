8d30229e941fcccded924549018b340f7fa204a4
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-02-05 16:32:36 -0600

    Reformatted

--- ABM-2d/ABM-2d-cpp/src/abmDualFrictionSwitch.cpp
+++ ABM-2d/ABM-2d-cpp/src/abmDualFrictionSwitch.cpp
@@ -21,18 +21,11 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     1/31/2024
+ *     2/5/2024
  */
 
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <cmath>
 #include <Eigen/Dense>
-#include <boost/random.hpp>
-#include "../include/growth.hpp"
-#include "../include/mechanics.hpp"
-#include "../include/switch.hpp"
+#include "../include/simulation.hpp"
 #include "../include/utils.hpp"
 
 using namespace Eigen;
@@ -41,29 +34,13 @@ using namespace Eigen;
 typedef double T;
 
 // Maximum number of attempts to control stepsize per Runge-Kutta iteration 
-const int max_tries = 3;
+const int max_tries_update_stepsize = 3;
 
 // Minimum error per Runge-Kutta iteration
 const T min_error = static_cast<T>(1e-30);
 
-// Minimum distance between neighboring cells
-const T min_dist = static_cast<T>(1e-8);
-
 int main(int argc, char** argv)
 {
-    // Define Butcher tableau for order 3(2) Runge-Kutta method by Bogacki
-    // and Shampine 
-    Array<T, Dynamic, Dynamic> A(4, 4); 
-    A << 0,     0,     0,     0,
-         1./2., 0,     0,     0,
-         0,     3./4., 0,     0,
-         2./9., 1./3., 4./9., 0;
-    Array<T, Dynamic, 1> b(4);
-    b << 2./9., 1./3., 4./9., 0;
-    Array<T, Dynamic, 1> bs(4); 
-    bs << 7./24., 1./4., 1./3., 1./8.;
-    T error_order = 2; 
-
     // Parse input json file 
     std::string json_filename = argv[1];
     boost::json::object json_data = parseConfigFile(json_filename).as_object();
@@ -73,213 +50,66 @@ int main(int argc, char** argv)
     const T Rcell = static_cast<T>(json_data["Rcell"].as_double());
     const T L0 = static_cast<T>(json_data["L0"].as_double());
     const T Ldiv = 2 * L0 + 2 * R;
-    const T growth_mean = static_cast<T>(json_data["growth_mean"].as_double());
-    const T growth_std = static_cast<T>(json_data["growth_std"].as_double());
-    const T eta_mean1 = static_cast<T>(json_data["eta_mean1"].as_double());
-    const T eta_std1 = static_cast<T>(json_data["eta_std1"].as_double()); 
-    const T eta_mean2 = static_cast<T>(json_data["eta_mean2"].as_double());
-    const T eta_std2 = static_cast<T>(json_data["eta_std2"].as_double());
-    const T lifetime_mean1 = static_cast<T>(json_data["lifetime_mean1"].as_double()); 
-    const T lifetime_mean2 = static_cast<T>(json_data["lifetime_mean2"].as_double()); 
     const T E0 = static_cast<T>(json_data["E0"].as_double());
     const T Ecell = static_cast<T>(json_data["Ecell"].as_double()); 
     const T sigma0 = static_cast<T>(json_data["sigma0"].as_double()); 
     const T eta_ambient = static_cast<T>(json_data["eta_ambient"].as_double()); 
-    T dt = static_cast<T>(json_data["max_dt"].as_double());    // Can be changed
-    const T max_stepsize = dt; 
+    const T max_stepsize = static_cast<T>(json_data["max_stepsize"].as_double()); 
     const int iter_write = json_data["iter_write"].as_int64(); 
     const int iter_update_stepsize = json_data["iter_update_stepsize"].as_int64(); 
     const int iter_update_neighbors = json_data["iter_update_neighbors"].as_int64(); 
     const T neighbor_threshold = 2 * (2 * R + L0);
+    const int max_iter = json_data["max_iter"].as_int64();
     const int n_cells = json_data["n_cells"].as_int64();
-    const T daughter_length_std = static_cast<T>(json_data["daughter_length_std"].as_double());
-    const T orientation_conc = static_cast<T>(json_data["orientation_conc"].as_double());
-    const T theta_bound = static_cast<T>(json_data["max_orientation_angle"].as_double());
-    const T max_error_allowed = static_cast<T>(json_data["max_rungekutta_error"].as_double());
+    const double growth_mean = json_data["growth_mean"].as_double();
+    const double growth_std = json_data["growth_std"].as_double();
+    const double eta_mean1 = json_data["eta_mean1"].as_double();
+    const double eta_std1 = json_data["eta_std1"].as_double(); 
+    const double eta_mean2 = json_data["eta_mean2"].as_double();
+    const double eta_std2 = json_data["eta_std2"].as_double();
+    const T lifetime_mean1 = static_cast<T>(json_data["lifetime_mean1"].as_double()); 
+    const T lifetime_mean2 = static_cast<T>(json_data["lifetime_mean2"].as_double()); 
+    const double daughter_length_std = json_data["daughter_length_std"].as_double();
+    const double daughter_angle_bound= json_data["daughter_angle_bound"].as_double();
+    const T max_error_allowed = static_cast<T>(json_data["max_error_allowed"].as_double());
 
-    // Surface contact area density and powers of cell radius  
-    const T surface_contact_density = std::pow(sigma0 * R * R / (4 * E0), 1. / 3.);
-    const T sqrtR = std::sqrt(R); 
-    const T powRdiff = std::pow(R - Rcell, 1.5);
-    Array<T, 4, 1> cell_cell_prefactors; 
-    cell_cell_prefactors << 2.5 * sqrtR,
-                            2.5 * E0 * sqrtR,
-                            E0 * powRdiff,
-                            Ecell; 
+    // Vectors of growth rate means and standard deviations (identical for
+    // both groups 
+    std::vector<double> growth_means { growth_mean, growth_mean };
+    std::vector<double> growth_stds  { growth_std, growth_std };
 
-    // Growth rate distribution function: normal distribution with given mean
-    // and standard deviation
-    boost::random::normal_distribution<> growth_dist(growth_mean, growth_std);
-    std::function<T(boost::random::mt19937&)> growth_dist_func =
-        [&growth_dist](boost::random::mt19937& rng)
-        {
-            return growth_dist(rng); 
-        };
+    // Vectors of friction coefficient means and standard deviations
+    const int switch_attribute = 9;
+    std::vector<double> attribute_means { eta_mean1, eta_mean2 };
+    std::vector<double> attribute_stds  { eta_std1, eta_std2 };
 
-    // Friction coefficient distribution functions: normal distributions with
-    // given mean and standard deviation 
-    boost::random::normal_distribution<> eta_dist1(eta_mean1, eta_std1);
-    boost::random::normal_distribution<> eta_dist2(eta_mean2, eta_std2); 
-    std::function<T(boost::random::mt19937&)> eta_dist1_func =
-        [&eta_dist1](boost::random::mt19937& rng)
-        {
-            return eta_dist1(rng); 
-        };
-    std::function<T(boost::random::mt19937&)> eta_dist2_func =
-        [&eta_dist2](boost::random::mt19937& rng)
-        {
-            return eta_dist2(rng);
-        };
-
-    // Daughter cell length ratio distribution function: normal distribution
-    // with mean 0.5 and given standard deviation
-    boost::random::normal_distribution<> daughter_length_dist(0.5, daughter_length_std); 
-    std::function<T(boost::random::mt19937&)> daughter_length_dist_func =
-        [&daughter_length_dist](boost::random::mt19937& rng)
-        {
-            return daughter_length_dist(rng); 
-        };
-
-    // Daughter angle distribution function: von Mises distribution with 
-    // mean 0 and given concentration parameter
-    boost::random::uniform_01<> uniform_dist;  
-    std::function<T(boost::random::mt19937&)> daughter_angle_dist_func =
-        [&orientation_conc, &uniform_dist, &theta_bound](boost::random::mt19937& rng)
-        {
-            T theta = vonMises<T>(0.0, orientation_conc, rng, uniform_dist);
-            while (theta > theta_bound || theta < -theta_bound)
-                theta = vonMises<T>(0.0, orientation_conc, rng, uniform_dist);
-            return theta;
-        };
-
-    // Rates of switching from group 1 to group 2 and vice versa 
-    T rate_12 = 1.0 / lifetime_mean1; 
-    T rate_21 = 1.0 / lifetime_mean2;
+    // Switching rates between groups 1 and 2
+    Array<T, Dynamic, Dynamic> switch_rates(2, 2); 
+    switch_rates << 0.0, 1.0 / lifetime_mean1,
+                    1.0 / lifetime_mean2, 0.0;
 
     // Output file prefix
-    std::string prefix = argv[2];
+    std::string outprefix = argv[2];
+
+    // Random seed
+    const int rng_seed = std::stoi(argv[3]);
 
     // Initialize simulation ...
     //
     // Define a founder cell at the origin at time zero, parallel to x-axis, 
     // with mean growth rate and default viscosity and friction coefficients
-    boost::random::mt19937 rng(std::stoi(argv[3]));
-    T t = 0; 
-    int i = 0;
-    int n = 1;
-    Array<T, Dynamic, Dynamic> cells(n, 11);
+    Array<T, Dynamic, Dynamic> cells(1, 11);
     cells << 0, 0, 1, 0, L0, L0 / 2, 0, growth_mean, eta_ambient, eta_mean1, 1;
-    Array<T, Dynamic, 4> velocities(n, 4);
-    velocities << 0, 0, 0, 0;
     
-    // Compute initial array of neighboring cells (should be empty)
-    Array<T, Dynamic, 6> neighbors = getCellNeighbors<T>(cells, neighbor_threshold, R, Ldiv);
-
-    // Write the founder cell to file
-    json_data["t_curr"] = t;
-    std::stringstream ss_init; 
-    ss_init << prefix << "_init.txt";
-    std::string filename_init = ss_init.str(); 
-    writeCells<T>(cells, json_data, filename_init); 
-    
-    // Run the simulation ... 
-    while (n < n_cells)
-    {
-        // Divide the cells that have reached division length
-        Array<int, Dynamic, 1> to_divide = divideMaxLength<T>(cells, Ldiv);
-        if (to_divide.sum() > 0)
-            std::cout << "... Dividing " << to_divide.sum() << " cells (iteration " << i
-                      << ")" << std::endl;
-        cells = divideCells<T>(
-            cells, t, R, Rcell, to_divide, growth_dist_func, rng,
-            daughter_length_dist_func, daughter_angle_dist_func
-        );
-
-        // Update the neighboring cells if division has occurred
-        if (to_divide.sum() > 0)
-            neighbors = getCellNeighbors<T>(cells, neighbor_threshold, R, Ldiv);
-
-        // Update cell positions and orientations 
-        auto result = stepRungeKuttaAdaptiveFromNeighbors<T>(
-            A, b, bs, cells, neighbors, dt, R, Rcell, cell_cell_prefactors,
-            surface_contact_density
-        ); 
-        Array<T, Dynamic, Dynamic> cells_new = std::get<0>(result); 
-        Array<T, Dynamic, 4> errors = std::get<1>(result);
-        Array<T, Dynamic, 4> velocities_new = std::get<2>(result); 
-
-        // If the error is big, retry the step with a smaller stepsize (up to
-        // a given maximum number of attempts)
-        if (i % iter_update_stepsize == 0)
-        {
-            T max_error = std::max(errors.abs().maxCoeff(), min_error); 
-            int j = 0; 
-            while (max_error > max_error_allowed && j < max_tries)
-            {
-                dt *= std::pow(max_error_allowed / max_error, 1.0 / (error_order + 1));
-                result = stepRungeKuttaAdaptiveFromNeighbors<T>(
-                    A, b, bs, cells, neighbors, dt, R, Rcell, cell_cell_prefactors,
-                    surface_contact_density
-                ); 
-                cells_new = std::get<0>(result); 
-                errors = std::get<1>(result);
-                velocities_new = std::get<2>(result); 
-                max_error = std::max(errors.abs().maxCoeff(), min_error); 
-                j++;  
-            }
-            // If the error is small, increase the stepsize up to a maximum stepsize
-            if (max_error < max_error_allowed)
-                dt = std::min(dt * std::pow(max_error_allowed / max_error, 1.0 / (error_order + 1)), max_stepsize);
-        }
-        cells = cells_new;
-        velocities = velocities_new;
-
-        // Grow the cells
-        growCells<T>(cells, dt, R);
-
-        // Update distances between neighboring cells
-        updateNeighborDistances<T>(cells, neighbors);
-
-        // Update current time 
-        t += dt;
-        i += 1;
-        n = cells.rows(); 
-
-        // Update neighboring cells 
-        if (i % iter_update_neighbors == 0)
-            neighbors = getCellNeighbors<T>(cells, neighbor_threshold, R, Ldiv);
-
-        // Switch cells between groups at the given rates
-        Array<int, Dynamic, 1> to_switch = chooseCellsToSwitch<T>(
-            cells, rate_12, rate_21, dt, rng, uniform_dist
-        );
-        switchGroups<T>(cells, 9, to_switch, eta_dist1_func, eta_dist2_func, rng);
-
-        // Write the current population to file
-        if (i % iter_write == 0)
-        {
-            std::cout << "Iteration " << i << ": " << n << " cells, time = "
-                      << t << ", max error = " << errors.abs().maxCoeff()
-                      << ", dt = " << dt << std::endl;
-            json_data["t_curr"] = t;
-            std::stringstream ss; 
-            ss << prefix << "_iter" << i << ".txt"; 
-            std::string filename = ss.str(); 
-
-            // Write the velocities to file as additional attributes
-            Array<T, Dynamic, Dynamic> cells_to_write(cells.rows(), cells.cols() + 4);
-            cells_to_write(Eigen::all, Eigen::seq(0, cells.cols() - 1)) = cells; 
-            cells_to_write(Eigen::all, Eigen::seq(cells.cols(), cells.cols() + 3)) = velocities;
-            writeCells<T>(cells_to_write, json_data, filename); 
-        } 
-    }
-
-    // Write final population to file
-    json_data["t_curr"] = t;
-    std::stringstream ss_final; 
-    ss_final << prefix << "_final.txt";
-    std::string filename_final = ss_final.str(); 
-    writeCells<T>(cells, json_data, filename_final); 
+    // Run the simulation
+    runSimulation<T>(
+        cells, max_iter, n_cells, R, Rcell, L0, Ldiv, E0, Ecell, sigma0, 
+        max_stepsize, true, outprefix, iter_write, iter_update_neighbors,
+        iter_update_stepsize, max_error_allowed, min_error,
+        max_tries_update_stepsize, neighbor_threshold, rng_seed, 2,
+        switch_attribute, growth_means, growth_stds, attribute_means, 
+        attribute_stds, switch_rates, daughter_length_std, daughter_angle_bound
+    ); 
     
     return 0; 
 }
