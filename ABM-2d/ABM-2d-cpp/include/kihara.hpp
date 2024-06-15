3a02386785f83cd4e58fcc4188d2743b3814a0ad
Kee Myoung Nam <kmnam@Kees-MBP.home>
2024-06-15 19:36:02 -0400

    Re-implemented Gay-Berne-Kihara

--- ABM-2d/ABM-2d-cpp/include/kihara.hpp
+++ ABM-2d/ABM-2d-cpp/include/kihara.hpp
@@ -3,11 +3,11 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     6/9/2024
+ *     6/15/2024
  */
 
-#ifndef KIHARA_POTENTIAL_FORCES_HPP
-#define KIHARA_POTENTIAL_FORCES_HPP
+#ifndef KIHARA_GBK_POTENTIAL_FORCES_HPP
+#define KIHARA_GBK_POTENTIAL_FORCES_HPP
 
 #include <Eigen/Dense>
 #include <boost/multiprecision/mpfr.hpp>
@@ -64,11 +64,11 @@ Matrix<T, 2, 4> attractiveForcesKihara2D(const Ref<const Matrix<T, 2, 1> >& r1,
  * This function returns chi^2. 
  */
 template <typename T>
-T aspectRatioParam(const T half_l1, const T half_l2, const T R)
+T squaredAspectRatioParam(const T half_l1, const T half_l2, const T R)
 {
-    T total_l1 = 2 * half_l1 + 2 * R; 
-    T total_l2 = 2 * half_l2 + 2 * R; 
-    T width = 2 * R;
+    T total_l1 = half_l1 + R; 
+    T total_l2 = half_l2 + R; 
+    T width = R;
     T total_l1_sq = total_l1 * total_l1; 
     T total_l2_sq = total_l2 * total_l2; 
     T width_sq = width * width; 
@@ -92,23 +92,22 @@ std::pair<T, Matrix<T, 2, 4> > anisotropyParamsGBK1(const Ref<const Matrix<T, 2,
                                                     const T half_l2, const T R,
                                                     const T exp)
 {
+    // Compute the anisotropy parameter
+    T chi2 = squaredAspectRatioParam<T>(half_l1, half_l2, R);
+    T n1_dot_n2 = n1.dot(n2);
+    T arg = 1.0 - chi2 * n1_dot_n2 * n1_dot_n2;
+    T eps = pow(arg, -0.5);
+
     // Get the partial derivative of the anisotropy parameter w.r.t each 
     // coordinate 
     Matrix<T, 2, 4> derivs = Matrix<T, 2, 4>::Zero();
 
-    // Compute the anisotropy parameter
-    T chi2 = aspectRatioParam<T>(half_l1, half_l2, R);
-    T n1_dot_n2 = n1.dot(n2);
-    T arg = 1.0 - chi2 * n1_dot_n2 * n1_dot_n2;
-    T eps = pow(arg, -0.5); 
+    // Compute the prefactor for each partial derivative 
+    T prefactor = exp * pow(eps, exp - 1) * chi2 * pow(arg, -1.5) * n1_dot_n2; 
 
-    // Compute the prefactor
-    T prefactor = 0;
-    if (exp == 1)
-        prefactor = pow(arg, -1.5) * chi2 * n1_dot_n2;
-    else if (exp > 0)
-        prefactor = exp * pow(arg, -0.5 * (exp - 1) - 1.5) * chi2 * n1_dot_n2;
-
+    // The partial derivatives w.r.t the cell center coordinates are
+    // uniformly zero 
+    //
     // Partial derivative w.r.t x-coordinate of cell 1 orientation
     derivs(0, 2) = prefactor * n2(0);
 
@@ -135,53 +134,62 @@ std::pair<T, Matrix<T, 2, 4> > anisotropyParamsGBK2(const Ref<const Matrix<T, 2,
                                                     const Ref<const Matrix<T, 2, 1> >& n2,
                                                     const T half_l2, const T R,
                                                     const Ref<const Matrix<T, 2, 1> >& d12,
-                                                    const T s, const T t,
-                                                    const T exp,
-                                                    const T well_depth_delta)
+                                                    const T exp, const T kappa0)
 {
-    // Compute the anisotropy parameter
-    T kappa = pow(well_depth_delta * (2 * min(half_l1, half_l2) + 2 * R) / R, 1 / exp); 
+    // Compute the anisotropy parameter ... 
+    //
+    // First compute the well-depth parameters \kappa and \chi'
+    T kappa = pow(kappa0 * ((min(half_l1, half_l2) / R) + 1), 1 / exp); 
     T chi = (kappa - 1) / (kappa + 1);
+
+    // Compute the vector r12 from r1 to r2 and its normalized vector 
+    Matrix<T, 2, 1> r12 = r2 - r1;
+    T r12_norm = r12.norm(); 
+    Matrix<T, 2, 1> r12n = r12 / r12_norm; 
+
+    // Compute the dot products (n1, n2), (r12n, n1), and (r12n, n2)
     T n1_dot_n2 = n1.dot(n2);
-    T d12_dot_n1 = d12.dot(n1); 
-    T d12_dot_n2 = d12.dot(n2); 
-    T dist = d12.norm();
-    T dist2 = dist * dist; 
-    Matrix<T, 2, 1> r12 = d12 / dist;
-    T r12_dot_n1 = r12.dot(n1); 
-    T r12_dot_n2 = r12.dot(n2);
-    T r12_dot_sum = r12_dot_n1 + r12_dot_n2; 
-    T r12_dot_diff = r12_dot_n1 - r12_dot_n2;
-    T numer1 = r12_dot_sum * r12_dot_sum; 
-    T numer2 = r12_dot_diff * r12_dot_diff; 
+    T r12n_dot_n1 = r12n.dot(n1); 
+    T r12n_dot_n2 = r12n.dot(n2);
+
+    // Compute the anisotropy parameter itself 
+    T r12n_dot_sum = r12n_dot_n1 + r12n_dot_n2; 
+    T r12n_dot_diff = r12n_dot_n1 - r12n_dot_n2;
+    T numer1 = r12n_dot_sum * r12n_dot_sum; 
+    T numer2 = r12n_dot_diff * r12n_dot_diff; 
     T denom1 = 1 + chi * n1_dot_n2; 
     T denom2 = 1 - chi * n1_dot_n2; 
     T eps = 1.0 - (chi / 2) * (numer1 / denom1 + numer2 / denom2);
 
-    // Compute the partial derivative of r12_dot_n1 and r12_dot_n2 with
-    // respect to each generalized coordinate 
-    Matrix<T, 2, 4> derivs1 = Matrix<T, 2, 4>::Zero(); 
-    derivs1(0, 0) = -n1(0) * dist + d12_dot_n1 * r12(0);
-    derivs1(0, 1) = -n1(1) * dist + d12_dot_n1 * r12(1);
-    derivs1(0, 2) = (-2 * s * n1(0) + t * n2(0)) * dist + s * d12_dot_n1 * r12(0); 
-    derivs1(0, 3) = (-2 * s * n1(1) + t * n2(1)) * dist + s * d12_dot_n1 * r12(1); 
+    // Compute the partial derivatives of (r12n, n1) with respect to each 
+    // generalized coordinate 
+    Matrix<T, 2, 4> derivs1 = Matrix<T, 2, 4>::Zero();
+    derivs1(0, 0) = (n1(0) * (-1 + r12n(0) * r12n(0)) + n1(1) * (r12n(0) * r12n(1))) / r12_norm;
+    derivs1(0, 1) = (n1(0) * (r12n(1) * r12n(0)) + n1(1) * (-1 + r12n(1) * r12n(1))) / r12_norm; 
+    derivs1(0, 2) = r12n(0); 
+    derivs1(0, 3) = r12n(1);
     derivs1(1, 0) = -derivs1(0, 0); 
     derivs1(1, 1) = -derivs1(0, 1); 
-    derivs1(1, 2) = t * (n1(0) * dist - d12_dot_n1 * r12(0)); 
-    derivs1(1, 3) = t * (n1(1) * dist - d12_dot_n1 * r12(1)); 
+
+    // Compute the partial derivatives of (r12n, n2) with respect to each 
+    // generalized coordinate 
     Matrix<T, 2, 4> derivs2 = Matrix<T, 2, 4>::Zero(); 
-    derivs2(0, 0) = -n2(0) * dist + d12_dot_n2 * r12(0); 
-    derivs2(0, 1) = -n2(1) * dist + d12_dot_n2 * r12(1);
-    derivs2(0, 2) = s * (-n2(0) * dist + d12_dot_n2 * r12(0));
-    derivs2(0, 3) = s * (-n2(1) * dist + d12_dot_n2 * r12(1));
+    derivs2(0, 0) = (n2(0) * (-1 + r12n(0) * r12n(0)) + n2(1) * (r12n(0) * r12n(1))) / r12_norm; 
+    derivs2(0, 1) = (n2(0) * (r12n(1) * r12n(0)) + n2(1) * (-1 + r12n(1) * r12n(1))) / r12_norm; 
     derivs2(1, 0) = -derivs2(0, 0); 
     derivs2(1, 1) = -derivs2(0, 1);
-    derivs2(1, 2) = (2 * t * n2(0) - s * n1(0)) * dist - t * d12_dot_n2 * r12(0);
-    derivs2(1, 3) = (2 * t * n2(1) - s * n1(1)) * dist - t * d12_dot_n2 * r12(1);
-    derivs1 /= dist2; 
-    derivs2 /= dist2;
+    derivs2(1, 2) = r12n(0); 
+    derivs2(1, 3) = r12n(1);
 
-    // Compute the partial derivative of n1_dot_n2 with respect to each
+    // Compute the partial derivatives of ((r12n, n1) + (r12n, n2))^2 with
+    // respect to each generalized coordinate 
+    Matrix<T, 2, 4> derivs_sum2 = 2 * r12n_dot_sum * (derivs1 + derivs2);
+
+    // Compute the partial derivatives of ((r12n, n1) - (r12n, n1))^2 with 
+    // respect to each generalized coordinate 
+    Matrix<T, 2, 4> derivs_diff2 = 2 * r12n_dot_diff * (derivs1 - derivs2); 
+
+    // Compute the partial derivative of (n1, n2) with respect to each 
     // generalized coordinate 
     Matrix<T, 2, 4> derivs3 = Matrix<T, 2, 4>::Zero(); 
     derivs3(0, 2) = n2(0);
@@ -191,13 +199,11 @@ std::pair<T, Matrix<T, 2, 4> > anisotropyParamsGBK2(const Ref<const Matrix<T, 2,
 
     // Compute the partial derivative of the anisotropy parameter with
     // respect to each generalized coordinate
-    Matrix<T, 2, 4> sum_derivs = derivs1 + derivs2; 
-    Matrix<T, 2, 4> diff_derivs = derivs1 - derivs2;
-    Matrix<T, 2, 4> term1 = r12_dot_sum * (2 * sum_derivs * denom1 - r12_dot_sum * chi * derivs3) / (denom1 * denom1);
-    Matrix<T, 2, 4> term2 = r12_dot_diff * (2 * diff_derivs * denom2 + r12_dot_diff * chi * derivs3) / (denom2 * denom2);
-    Matrix<T, 2, 4> derivs = Matrix<T, 2, 4>::Ones() - 0.5 * chi * (term1 + term2); 
+    Matrix<T, 2, 4> term1 = ((denom1 * derivs_sum2) - (numer1 * chi * derivs3)) / (denom1 * denom1);
+    Matrix<T, 2, 4> term2 = ((denom2 * derivs_diff2) + (numer2 * chi * derivs3)) / (denom2 * denom2); 
+    Matrix<T, 2, 4> derivs = -0.5 * chi * (term1 + term2); 
 
-    return std::make_pair(eps, derivs); 
+    return std::make_pair(eps, exp * pow(eps, exp - 1) * derivs); 
 }
 
 /**
@@ -212,7 +218,7 @@ Matrix<T, 2, 4> attractiveForcesGBK2D(const Ref<const Matrix<T, 2, 1> >& r1,
                                       const T half_l2, const T R, 
                                       const Ref<const Matrix<T, 2, 1> >& d12, 
                                       const T s, const T t, const T exp1,
-                                      const T exp2, const T well_depth_delta)
+                                      const T exp2, const T kappa0)
 {
     // Get the anisotropy parameters and their partial derivatives 
     auto result1 = anisotropyParamsGBK1<T>(
@@ -221,7 +227,7 @@ Matrix<T, 2, 4> attractiveForcesGBK2D(const Ref<const Matrix<T, 2, 1> >& r1,
     T eps1 = result1.first; 
     Matrix<T, 2, 4> deps1 = result1.second; 
     auto result2 = anisotropyParamsGBK2<T>(
-        r1, n1, half_l1, r2, n2, half_l2, R, d12, s, t, exp2, well_depth_delta
+        r1, n1, half_l1, r2, n2, half_l2, R, d12, exp2, kappa0
     );
     T eps2 = result2.first; 
     Matrix<T, 2, 4> deps2 = result2.second; 
