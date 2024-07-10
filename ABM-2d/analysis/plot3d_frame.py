b625a18d6bf36786616f19585b28bbcd00dadc25
Kee Myoung Nam <kn449@login1.mccleary.ycrc.yale.edu>
2024-07-10 13:59:22 -0400

    First commits

---
+++ ABM-2d/analysis/plot3d_frame.py
@@ -0,0 +1,93 @@
+"""
+Functions for visualizing spherocylindrical cells in 3-D with Pyvista. 
+
+Authors:
+    Kee-Myoung Nam
+
+Last updated:
+    7/10/2024
+"""
+
+import sys
+import numpy as np
+import pyvista as pv
+pv.start_xvfb()
+import seaborn as sns
+from utils import read_cells
+from plot3d import plot_cells 
+
+#######################################################################
+def plot_frame(filename, outfilename, view='xy', res=50, uniform_color=False):
+    """
+    Given an ordered list of files containing cells to be plotted, parse 
+    and plot each population of cells and generate a video. 
+
+    Parameters
+    ----------
+    filename : str
+        Path to file containing the cells to be plotted.
+    outfilename : str
+        Output filename. 
+    view : str, 'xy' or 'xz' or 'yz'
+        Set the view to the given pair of axes. 
+    res : int
+        Resolution for plotting each cylinder and hemisphere.
+    uniform_color : bool
+        If True, color all cells with a single color (blue). 
+    """
+    palette = [    # Assume a maximum of five groups 
+        sns.color_palette('hls', 8)[5],
+        sns.color_palette('hls', 8)[0],
+        sns.color_palette('hls', 8)[3],
+        sns.color_palette('hls', 8)[1],
+        sns.color_palette('hls', 8)[6]
+    ]
+    
+    # Parse the cells and infer axes limits
+    cells, params = read_cells(filename)
+    R = params['R']
+    L0 = params['L0']
+    E0 = params['E0']
+    sigma0 = params['sigma0']
+    rz = R - (1 / R) * ((R * R * sigma0) / (4 * E0)) ** (2 / 3)
+    xmin = np.floor(cells[:, 0].min() - 4 * L0)
+    xmax = np.ceil(cells[:, 0].max() + 4 * L0)
+    ymin = np.floor(cells[:, 1].min() - 4 * L0)
+    ymax = np.ceil(cells[:, 1].max() + 4 * L0)
+    zmin = rz - R
+    zmax = rz + R
+
+    # Determine cell colors 
+    if uniform_color:
+        colors = [palette[0] for i in range(cells.shape[0])]
+    else:
+        ngroups = int(max(cells[:, 10]))
+        palette_ = palette[:ngroups]
+        colors = [
+            palette_[int(cells[i, 10]) - 1] for i in range(cells.shape[0])
+        ]
+
+    # Plot the cells 
+    title = 't = {:.10f}, n = {}'.format(params['t_curr'], cells.shape[0])
+    print('Plotting {} ({} cells) ...'.format(filename, cells.shape[0]))
+    pl = pv.Plotter(off_screen=True)
+    pl = plot_cells(
+        cells, pl, R, rz, colors, xmin, xmax, ymin, ymax, zmin, zmax,
+        title, view=view, res=res
+    )
+
+    # Get a screenshot of the plotted cells
+    image = Image.fromarray(pl.screenshot())
+    print('... saving to {}'.format(outfilename))
+    image.save(outfilename)
+    pl.close()
+
+#######################################################################
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    outfilename = sys.argv[2]
+    uniform_color = (len(sys.argv) == 4 and sys.argv[3] == '--uniform-color')
+    plot_frame(
+        filename, outfilename, view='xy', res=20, uniform_color=uniform_color
+    )
+
---
+++ ABM-2d/analysis/plot3d_video.py
@@ -0,0 +1,72 @@
+"""
+Functions for visualizing spherocylindrical cells in 3-D with Pyvista. 
+
+Authors:
+    Kee-Myoung Nam
+
+Last updated:
+    7/10/2024
+"""
+
+import sys
+import numpy as np
+import pyvista as pv
+pv.start_xvfb()
+import seaborn as sns
+from utils import read_cells, parse_dir
+from plot3d import plot_simulation
+
+#######################################################################
+if __name__ == '__main__':
+    inprefix = sys.argv[1]
+    outprefix = sys.argv[2]
+    nframes_total = int(sys.argv[3])
+    nframes_per_video = int(sys.argv[4])
+    uniform_color = (len(sys.argv) == 6 and sys.argv[5] == '--uniform-color')
+    filenames = parse_dir(inprefix)
+    print('Parsing {} files ...'.format(len(filenames)))
+
+    # Get cell radius, final dimensions, and final timepoint from final file
+    cells, params = read_cells(filenames[-1])
+    R = params['R']
+    L0 = params['L0']
+    E0 = params['E0']
+    sigma0 = params['sigma0']
+    rz = R - (1 / R) * ((R * R * sigma0) / (4 * E0)) ** (2 / 3)
+    xmin = np.floor(cells[:, 0].min() - 4 * L0)
+    xmax = np.ceil(cells[:, 0].max() + 4 * L0)
+    ymin = np.floor(cells[:, 1].min() - 4 * L0)
+    ymax = np.ceil(cells[:, 1].max() + 4 * L0)
+    zmin = rz - R
+    zmax = rz + R
+    t_final = params['t_curr']
+
+    # Generate array of timepoints
+    timepoints = np.linspace(0, t_final, nframes_total)
+    
+    # Run through the files and identify, for each timepoint, the file 
+    # whose timepoint is closest
+    file_timepoints = []
+    for filename in filenames:
+        _, params = read_cells(filename)
+        file_timepoints.append(params['t_curr'])
+    filenames_nearest = []
+    for t in timepoints:
+        nearest_idx = np.argmin(np.abs(file_timepoints - t))
+        filenames_nearest.append(filenames[nearest_idx])
+
+    # Plot the simulation in fixed increments
+    start = 0
+    end = nframes_per_video
+    i = 0
+    while end <= nframes_total:
+        plot_simulation(
+            filenames_nearest[start:end], outprefix + '_{}.avi'.format(i), R,
+            rz, xmin, xmax, ymin, ymax, zmin, zmax, view='xy', res=20, fps=20,
+            uniform_color=uniform_color
+        )
+        print('Saving video: {}_{}.avi'.format(outprefix, i))
+        start += nframes_per_video
+        end += nframes_per_video
+        i += 1
+
