b1df462b2636537a4a1f77bd942e63ab8485b443
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-08-07 23:58:37 -0400

    Updated

--- ABM-2d/ABM-2d-cpp/src/abmDualGrowthFrictionSwitch.cpp
+++ ABM-2d/ABM-2d-cpp/src/abmDualGrowthFrictionSwitch.cpp
@@ -3,29 +3,31 @@
  * states that exhibit different growth rates and friction coefficients.
  *
  * In what follows, a population of N cells is represented as a 2-D array of 
- * size (N, 11), where each row represents a cell and stores the following data:
+ * size (N, 12), where each row represents a cell and stores the following data:
  * 
- * 0) x-coordinate of cell center
- * 1) y-coordinate of cell center
- * 2) x-coordinate of cell orientation vector
- * 3) y-coordinate of cell orientation vector
- * 4) cell length (excluding caps) 
- * 5) half of cell length (excluding caps)
- * 6) timepoint at which the cell was formed
- * 7) cell growth rate
- * 8) cell's ambient viscosity with respect to surrounding fluid
- * 9) cell-surface friction coefficient
- * 10) cell group identifier (1 for slow-growing/high-friction, 2 for
+ * 0) cell ID
+ * 1) x-coordinate of cell center
+ * 2) y-coordinate of cell center
+ * 3) x-coordinate of cell orientation vector
+ * 4) y-coordinate of cell orientation vector
+ * 5) cell length (excluding caps)
+ * 6) half of cell length (excluding caps)
+ * 7) timepoint at which the cell was formed
+ * 8) cell growth rate
+ * 9) cell's ambient viscosity with respect to surrounding fluid
+ * 10) cell-surface friction coefficient
+ * 11) cell group identity (1 for slow-growing/high-friction, 2 for
  *     fast-growing/low-friction)
  *
  * Authors:
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     2/26/2024
+ *     8/1/2024
  */
 
 #include <Eigen/Dense>
+#include "../include/indices.hpp"
 #include "../include/simulation.hpp"
 #include "../include/utils.hpp"
 
@@ -58,7 +60,8 @@ int main(int argc, char** argv)
     const T max_stepsize = static_cast<T>(json_data["max_stepsize"].as_double()); 
     const int iter_write = json_data["iter_write"].as_int64(); 
     const int iter_update_stepsize = json_data["iter_update_stepsize"].as_int64(); 
-    const int iter_update_neighbors = json_data["iter_update_neighbors"].as_int64(); 
+    const int iter_update_neighbors = json_data["iter_update_neighbors"].as_int64();
+    const int iter_update_boundary = 0;
     const T neighbor_threshold = 2 * (2 * R + L0);
     const int max_iter = json_data["max_iter"].as_int64();
     const int n_cells = json_data["n_cells"].as_int64();
@@ -75,6 +78,12 @@ int main(int argc, char** argv)
     const T daughter_length_std = static_cast<T>(json_data["daughter_length_std"].as_double());
     const T daughter_angle_bound = static_cast<T>(json_data["daughter_angle_bound"].as_double());
     const T max_error_allowed = static_cast<T>(json_data["max_error_allowed"].as_double());
+    const AdhesionMode adhesion_mode = AdhesionMode::NONE;    // No cell-cell adhesion
+    std::unordered_map<std::string, T> adhesion_params;
+    const bool confine = false;    // No radial confinement forces 
+    std::unordered_map<std::string, T> confine_params; 
+    const GrowthVoidMode growth_void_mode = GrowthVoidMode::NONE;    // No growth void 
+    std::unordered_map<std::string, T> growth_void_params; 
 
     // Vectors of growth rate means and standard deviations
     Array<T, Dynamic, 1> growth_means(2); 
@@ -83,7 +92,7 @@ int main(int argc, char** argv)
     growth_stds << growth_std1, growth_std2; 
 
     // Vectors of friction coefficient means and standard deviations
-    std::vector<int> switch_attributes { 9 };
+    std::vector<int> switch_attributes { __colidx_eta1 };
     Array<T, Dynamic, Dynamic> attribute_means(2, 1);
     Array<T, Dynamic, Dynamic> attribute_stds(2, 1);
     attribute_means << eta_mean1, eta_mean2;
@@ -104,17 +113,23 @@ int main(int argc, char** argv)
     //
     // Define a founder cell at the origin at time zero, parallel to x-axis, 
     // with mean growth rate and default viscosity and friction coefficients
-    Array<T, Dynamic, Dynamic> cells(1, 11);
-    cells << 0, 0, 1, 0, L0, L0 / 2, 0, growth_mean1, eta_ambient, eta_mean1, 1;
+    Array<T, Dynamic, Dynamic> cells(1, 12);
+    cells << 0, 0, 0, 1, 0, L0, L0 / 2, 0, growth_mean1, eta_ambient, eta_mean1, 1;
+
+    // Initialize parent IDs 
+    std::vector<int> parents; 
+    parents.push_back(-1); 
     
     // Run the simulation
     runSimulation<T>(
-        cells, max_iter, n_cells, R, Rcell, L0, Ldiv, E0, Ecell, sigma0, 
+        cells, parents, max_iter, n_cells, R, Rcell, L0, Ldiv, E0, Ecell, sigma0, 
         max_stepsize, true, outprefix, iter_write, iter_update_neighbors,
-        iter_update_stepsize, max_error_allowed, min_error,
-        max_tries_update_stepsize, neighbor_threshold, rng_seed, 2,
+        iter_update_boundary, iter_update_stepsize, max_error_allowed,
+        min_error, max_tries_update_stepsize, neighbor_threshold, rng_seed, 2,
         switch_attributes, growth_means, growth_stds, attribute_means, 
-        attribute_stds, switch_rates, daughter_length_std, daughter_angle_bound
+        attribute_stds, switch_rates, daughter_length_std, daughter_angle_bound,
+        adhesion_mode, adhesion_params, confine, confine_params, growth_void_mode,
+        growth_void_params
     ); 
     
     return 0; 
