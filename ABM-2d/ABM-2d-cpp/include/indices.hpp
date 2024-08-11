ccf9dec1a56f3c75a9646acb4cdbe06c167f07c6
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-08-11 00:27:16 -0400

    Updated to include velocities

--- ABM-2d/ABM-2d-cpp/include/indices.hpp
+++ ABM-2d/ABM-2d-cpp/include/indices.hpp
@@ -3,7 +3,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     8/1/2024
+ *     8/10/2024
  */
 
 #ifndef CELL_ARRAY_INDEX_DECLARATIONS_HPP
@@ -21,13 +21,18 @@ const int __colidx_nx = 3;
 const int __colidx_ny = 4; 
 const ArithmeticSequence<> __colseq_n = Eigen::seq(3, 4);
 const ArithmeticSequence<> __colseq_coords = Eigen::seq(1, 4);
-const int __colidx_l = 5; 
-const int __colidx_half_l = 6; 
-const int __colidx_t0 = 7; 
-const int __colidx_growth = 8; 
-const int __colidx_eta0 = 9; 
-const int __colidx_eta1 = 10;
-const int __colidx_group = 11; 
-const int __colidx_plasmid = 12;
+const int __colidx_drx = 5; 
+const int __colidx_dry = 6; 
+const int __colidx_dnx = 7; 
+const int __colidx_dny = 8;
+const ArithmeticSequence<> __colseq_velocities = Eigen::seq(5, 8); 
+const int __colidx_l = 9; 
+const int __colidx_half_l = 10; 
+const int __colidx_t0 = 11; 
+const int __colidx_growth = 12; 
+const int __colidx_eta0 = 13; 
+const int __colidx_eta1 = 14;
+const int __colidx_group = 15; 
+const int __colidx_plasmid = 16;
 
 #endif 
