8860097d4c58617d8eeded309163f71d14cc9257
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-08-28 13:19:05 -0400

    Small updates

--- ABM-2d/ABM-2d-cpp/include/indices.hpp
+++ ABM-2d/ABM-2d-cpp/include/indices.hpp
@@ -3,7 +3,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     8/23/2024
+ *     8/27/2024
  */
 
 #ifndef CELL_ARRAY_INDEX_DECLARATIONS_HPP
@@ -23,9 +23,11 @@ const int __colidx_ny = 4;
 const ArithmeticSequence<> __colseq_n = Eigen::seq(3, 4);
 const ArithmeticSequence<> __colseq_coords = Eigen::seq(1, 4);
 const int __colidx_drx = 5; 
-const int __colidx_dry = 6; 
+const int __colidx_dry = 6;
+const ArithmeticSequence<> __colseq_dr = Eigen::seq(5, 6); 
 const int __colidx_dnx = 7; 
 const int __colidx_dny = 8;
+const ArithmeticSequence<> __colseq_dn = Eigen::seq(7, 8); 
 const ArithmeticSequence<> __colseq_velocities = Eigen::seq(5, 8); 
 const int __colidx_l = 9; 
 const int __colidx_half_l = 10; 
@@ -35,7 +37,7 @@ const int __colidx_eta0 = 13;
 const int __colidx_eta1 = 14;
 const int __colidx_group = 15;
 const std::vector<int> __colvec_required {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 }; 
 
 #endif 
