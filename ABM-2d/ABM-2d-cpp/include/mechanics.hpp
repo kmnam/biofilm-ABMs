852c6858d603d19dbe29af3e22ee53ba7cd4eb0d
Kee Myoung Nam <kmnam.99.12@gmail.com>
2024-10-12 15:12:15 -0400

    Edited docstring

--- ABM-2d/ABM-2d-cpp/include/mechanics.hpp
+++ ABM-2d/ABM-2d-cpp/include/mechanics.hpp
@@ -1,10 +1,10 @@
 /**
  * Implementations of cell-cell and cell-surface interaction forces.
  *
- * In what follows, a population of N cells is represented as a 2-D array of
- * size (N, 16+), whose columns are as specified in `indices.hpp`.
+ * In what follows, a population of N cells is represented as a 2-D array
+ * with N rows, whose columns are as specified in `indices.hpp`.
  *
- * Additional features may be included in the array but these are not relevant
+ * Additional columns may be included in the array but these are not relevant
  * for the computations implemented here. 
  *
  * Authors:
