2341eb1b08693f2130fcb35bfae282f1cda49cc5
Kee Myoung Nam <kmnam.99.12@gmail.com>
2025-03-22 16:57:44 -0400

    First commit

--- ABM-2d/analysis/plot3d_frame.py
+++ ABM-2d/analysis/plot3d_frame.py
@@ -1,32 +1,47 @@
 """
-Functions for visualizing spherocylindrical cells in 3-D with Pyvista. 
+Plot a single frame using spherocylinders in 3-D with Pyvista. 
 
 Authors:
     Kee-Myoung Nam
 
 Last updated:
-    7/30/2024
+    3/22/2025
 """
 
+import gc
 import sys
+from time import sleep
 import pyvista as pv
+from utils import read_cells
 from plot3d import plot_frame
 
 #######################################################################
 if __name__ == '__main__':
     filename = sys.argv[1]
     outfilename = sys.argv[2]
+    xmin, xmax, ymin, ymax = [float(x) for x in sys.argv[3:7]]
+    if len(sys.argv[3:]) == 4:
+        position = None
+    else:
+        values = [float(x) for x in sys.argv[7].strip('()').split(',')]
+        position = [values[:3], values[3:6], values[6:]]
     args = sys.argv[3:]
     uniform_color = ('--uniform' in args)
     plot_boundary = ('--bound' in args)
     plot_membrane = ('--membrane' in args)
     plot_arrested = ('--arrested' in args)
-    plot_3d = ('--3d' in args)
-    pl = pv.Plotter(off_screen=True)
-    #pl.clear()
+    _, params = read_cells(filename)
+    time = params['t_curr']
+    xmin -= params['L0']
+    ymin -= params['L0']
+    xmax += params['L0']
+    ymax += params['L0']
     plot_frame(
-        filename, outfilename, pl, plot_3d=plot_3d, view='xy', res=20,
-        uniform_color=uniform_color, plot_boundary=plot_boundary,
-        plot_membrane=plot_membrane, plot_arrested=plot_arrested
+        filename, outfilename, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
+        res=20, time=time, image_scale=5, uniform_color=uniform_color,
+        plot_boundary=plot_boundary, plot_membrane=plot_membrane,
+        plot_arrested=plot_arrested, position=position, show=False
     )
+    gc.collect()
+    sleep(1)
 
