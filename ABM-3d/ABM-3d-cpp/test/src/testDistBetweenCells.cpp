aba7f29dbd436c1ba9db3b3df24054a60cf3b2d4
Kee Myoung Nam <kmnam.99.12@gmail.com>
2025-04-13 13:01:54 -0400

    Updated tests to include 3-D configurations

--- ABM-3d/ABM-3d-cpp/test/src/testDistBetweenCells.cpp
+++ ABM-3d/ABM-3d-cpp/test/src/testDistBetweenCells.cpp
@@ -6,10 +6,11 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     2/22/2025
+ *     4/13/2025
  */
 #include <cmath>
 #include <Eigen/Dense>
+#include <boost/math/constants/constants.hpp>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Segment_3.h>
 #include <catch2/catch_test_macros.hpp>
@@ -20,10 +21,54 @@ using namespace Eigen;
 
 typedef double T;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K; 
-typedef K::Segment_3 Segment_3; 
+typedef K::Segment_3 Segment_3;
 
+using std::sin; 
+using boost::multiprecision::sin; 
+using std::cos; 
+using boost::multiprecision::cos; 
+using std::tan; 
+using boost::multiprecision::tan;
+
+/* ---------------------------------------------------------------- //
+ *                         UTILITY FUNCTIONS                        //
+ * ---------------------------------------------------------------- */
 /**
- * A generic test function for nearestCellBodyCoordToPoint(). 
+ * A function that calculates the shortest distance vector between two 
+ * infinite lines. 
+ *
+ * This was taken from Vega and Lago, Comput. Chem. (1994). 
+ */
+std::tuple<Matrix<T, 3, 1>, T, T> distBetweenLines(const Ref<const Matrix<T, 3, 1> >& r1,
+                                                   const Ref<const Matrix<T, 3, 1> >& n1,
+                                                   const Ref<const Matrix<T, 3, 1> >& r2,
+                                                   const Ref<const Matrix<T, 3, 1> >& n2)
+{
+    Matrix<T, 3, 1> r12 = r2 - r1; 
+    T s = (r12.dot(n1) - n1.dot(n2) * r12.dot(n2)) / (1.0 - n1.dot(n2) * n1.dot(n2));
+    T t = (-r12.dot(n2) + n1.dot(n2) * r12.dot(n1)) / (1.0 - n1.dot(n2) * n1.dot(n2));
+    Matrix<T, 3, 1> d = r12 + t * n2 - s * n1;
+
+    return std::make_tuple(d, s, t);  
+}
+
+/* ---------------------------------------------------------------- //
+ *                          TEST FUNCTIONS                          //
+ * ---------------------------------------------------------------- */
+/**
+ * A generic test function for nearestCellBodyCoordToPoint().
+ *
+ * @param rx x-coordinate of cell center. 
+ * @param ry y-coordinate of cell center.
+ * @param rz z-coordinate of cell center. 
+ * @param nx x-coordinate of cell orientation. 
+ * @param ny y-coordinate of cell orientation.
+ * @param nz z-coordinate of cell orientation.
+ * @param half_l Cell half-length.
+ * @param qx x-coordinate of query point. 
+ * @param qy y-coordinate of query point. 
+ * @param qz z-coordinate of query point.
+ * @param target Pre-computed nearest centerline coordinate. 
  */
 void testNearestCellBodyCoordToPoint(const T rx, const T ry, const T rz,
                                      const T nx, const T ny, const T nz,
@@ -89,6 +134,9 @@ void testDistBetweenCells(const T r1x, const T r1y, const T r1z, const T n1x,
     REQUIRE_THAT(std::get<2>(result), Catch::Matchers::WithinAbs(target_t, 1e-8)); 
 }
 
+/* ---------------------------------------------------------------- //
+ *                           TEST MODULES                           //
+ * ---------------------------------------------------------------- */
 /**
  * A series of tests for nearestCellBodyCoordToPoint(). 
  */
@@ -122,9 +170,11 @@ TEST_CASE("Tests for nearestCellBodyCoordToPoint()", "[nearestCellBodyCoordToPoi
  */
 TEST_CASE("Tests for distBetweenCells(), skew cells", "[distBetweenCells()]")
 {
-    /* -------------------------------------------------------------- //
-     *                     HORIZONTAL TEST CASES                      //
-     * -------------------------------------------------------------- */
+    T target_dx, target_dy, target_dz, target_s, target_t; 
+
+    // -------------------------------------------------------------- //
+    //                     HORIZONTAL TEST CASES                      //
+    // -------------------------------------------------------------- //
     // r1 = (2, 1, 0), n1 = (0.6, 0.8, 0), l1 = 2
     // r2 = (0, 3, 0), n2 = (0, 1, 0), l2 = 1
     //
@@ -141,11 +191,11 @@ TEST_CASE("Tests for distBetweenCells(), skew cells", "[distBetweenCells()]")
     //
     // The shortest distance between the two cells is achieved at t = 0.5
     // and s = (13 / 5) - (12 / 13) * (6.4 - 3 - 0.5) ~= -0.07692307692
-    T target_s = -0.07692307692; 
-    T target_t = 0.5; 
-    T target_dx = (5 + target_t * 0) - (4 + target_s * 5.0 / 13.0);
-    T target_dy = (3 + target_t * 1) - (4 + target_s * 12.0 / 13.0);
-    T target_dz = 0; 
+    target_s = -0.07692307692; 
+    target_t = 0.5; 
+    target_dx = (5 + target_t * 0) - (4 + target_s * 5.0 / 13.0);
+    target_dy = (3 + target_t * 1) - (4 + target_s * 12.0 / 13.0);
+    target_dz = 0; 
     testDistBetweenCells(
         4, 4, 2, 5.0 / 13.0, 12.0 / 13.0, 0, 2.7,    // Cell 1
         5, 3, 2, 0, 1, 0, 0.5,                       // Cell 2
@@ -192,6 +242,101 @@ TEST_CASE("Tests for distBetweenCells(), skew cells", "[distBetweenCells()]")
         6, 2, 5, 0, 1, 0, 0.5,                       // Cell 2
         target_dx, target_dy, target_dz, target_s, target_t
     );
+
+    // -------------------------------------------------------------- //
+    //                         3-D TEST CASES                         //
+    // -------------------------------------------------------------- //
+    // r1 = (0, 0, 0), n1 = (1, 0, 0), l1 = 1
+    // r2 = (0, 1.4, 0), n2 = (0, 0, 1), l2 = 1
+    //
+    // The shortest distance between the two cells is achieved at their
+    // centers, s = t = 0, along the vector (0, 1.4, 0)
+    target_s = 0; 
+    target_t = 0; 
+    target_dx = 1.4; 
+    target_dy = 0; 
+    target_dz = 0; 
+    testDistBetweenCells(
+        0, 0, 0, 1, 0, 0, 0.5,      // Cell 1
+        0, 1.4, 0, 0, 0, 1, 0.5,    // Cell 2
+        0, 1.4, 0, 0, 0
+    );
+
+    // A skew configuration in which cell 2 runs along the y-axis and cell 1
+    // runs diagonally "on top" of cell 2, within the xz-plane
+    //
+    // r1 = (0, 0, l1 * sin(30) / 2), n1 = (cos(30), 0, sin(30)), l1 = 1
+    // r2 = (0.75 * l1 / cos(30) - l1 * cos(30) / 2, 0, 0), n2 = (0, 1, 0), l2 = 1
+    //
+    // The shortest distance between the two cells is achieved at:
+    //
+    // s = 0.25 * l1 = 0.25 and t = 0,
+    //
+    // along the vector (0.75 / cos(30) - 0.75 * cos(30), 0, -0.75 * sin(30)),
+    // which is equal to (0.75 * tan(30) * sin(30), 0, -0.75 * sin(30))
+    T half_l1 = 0.5; 
+    T half_l2 = 0.5; 
+    T l1 = 2 * half_l1; 
+    T l2 = 2 * half_l2; 
+    T theta = boost::math::constants::sixth_pi<T>(); 
+    target_s = 0.25 * l1;
+    target_t = 0; 
+    target_dx = 0.75 * tan(theta) * sin(theta); 
+    target_dy = 0; 
+    target_dz = -0.75 * sin(theta); 
+    testDistBetweenCells(
+        0, 0, half_l1 * sin(theta), cos(theta), 0, sin(theta), 0.5,           // Cell 1
+        0.75 * l1 / cos(theta) - half_l1 * cos(theta), 0, 0, 0, 1, 0, 0.5,    // Cell 2
+        target_dx, target_dy, target_dz, target_s, target_t
+    );
+
+    // The same configuration but with cell 2 translated along the y-direction
+    //
+    // r1 = (0, 0, l1 * sin(30) / 2), n1 = (cos(30), 0, sin(30)), l1 = 1
+    // r2 = (0.75 * l1 / cos(30) - l1 * cos(30) / 2, l2 / 4, 0), n2 = (0, 1, 0), l2 = 1
+    //
+    // The shortest distance between the two cells is achieved at:
+    //
+    // s = 0.25 * l1 = 0.25 and t = -0.25 * l2 = -0.25,
+    //
+    // along the vector (0.75 / cos(30) - 0.75 * cos(30), 0, -0.75 * sin(30)),
+    // which is equal to (0.75 * tan(30) * sin(30), 0, -0.75 * sin(30))
+    target_s = 0.25 * l1;
+    target_t = -0.25 * l2; 
+    target_dx = 0.75 * tan(theta) * sin(theta); 
+    target_dy = 0; 
+    target_dz = -0.75 * sin(theta); 
+    testDistBetweenCells(
+        0, 0, half_l1 * sin(theta), cos(theta), 0, sin(theta), 0.5,                // Cell 1
+        0.75 * l1 / cos(theta) - half_l1 * cos(theta), l2 / 4, 0, 0, 1, 0, 0.5,    // Cell 2
+        target_dx, target_dy, target_dz, target_s, target_t
+    ); 
+
+    // The same configuration but with cell 2 recentered to r(1) = 0, but 
+    // with the orientation perturbed slightly within the xy-plane 
+    //
+    // r1 = (0, 0, l1 * sin(30) / 2), n1 = (cos(30), 0, sin(30)), l1 = 1
+    // r2 = (0.75 * l1 / cos(30) - l1 * cos(30) / 2, 0, 0), n2 = (-sin(5), cos(5), 0), l2 = 1
+    //
+    // We can compute the shortest distance between the two cells as the 
+    // shortest distance between the two extended lines 
+    T eps = 5 * boost::math::constants::pi<T>() / 180;
+    Matrix<T, 3, 1> r1, n1, r2, n2; 
+    r1 << 0, 0, half_l1 * sin(theta);
+    n1 << cos(theta), 0, sin(theta);
+    r2 << 0.75 * l1 / cos(theta) - half_l1 * cos(theta), 0, 0;
+    n2 << -sin(eps), cos(eps), 0; 
+    auto target = distBetweenLines(r1, n1, r2, n2); 
+    target_s = std::get<1>(target); 
+    target_t = std::get<2>(target); 
+    target_dx = std::get<0>(target)(0);
+    target_dy = std::get<0>(target)(1); 
+    target_dz = std::get<0>(target)(2); 
+    testDistBetweenCells(
+        0, 0, half_l1 * sin(theta), cos(theta), 0, sin(theta), 0.5,                          // Cell 1
+        0.75 * l1 / cos(theta) - half_l1 * cos(theta), 0, 0, -sin(eps), cos(eps), 0, 0.5,    // Cell 2
+        target_dx, target_dy, target_dz, target_s, target_t
+    ); 
 }
 
 /**
