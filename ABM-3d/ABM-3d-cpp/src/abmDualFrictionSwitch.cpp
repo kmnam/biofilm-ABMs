38ddae8e5b2c76822f0d7944bbb840190365189e
Kee Myoung Nam <kmnam.99.12@gmail.com>
2025-04-28 11:38:33 -0400

    Integrated

--- ABM-3d/ABM-3d-cpp/src/abmDualFrictionSwitch.cpp
+++ ABM-3d/ABM-3d-cpp/src/abmDualFrictionSwitch.cpp
@@ -1,6 +1,7 @@
 /**
  * An agent-based model of 3-D biofilm growth that switches cells between two
- * states that exhibit different friction coefficients. 
+ * states that exhibit different cell-surface adhesion energies and/or friction
+ * coefficients. 
  *
  * In what follows, a population of N cells is represented as a 2-D array 
  * with N rows, whose columns are as specified in `include/indices.hpp`.
@@ -9,7 +10,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     3/12/2025
+ *     4/28/2025
  */
 
 #include <Eigen/Dense>
@@ -34,6 +35,23 @@ int main(int argc, char** argv)
     std::string json_filename = argv[1];
     boost::json::object json_data = parseConfigFile(json_filename).as_object();
 
+    // Check if the simulation varies just the cell-surface adhesion energy
+    // density (default), the cell-surface friction coefficient (--coef), or 
+    // both (--combined)
+    int friction_mode = 0; 
+    if (argc > 4)
+    {
+        std::string arg = argv[4];
+        if (arg == "--coef")
+            friction_mode = 1;
+        else if (arg == "--combined")
+            friction_mode = 2; 
+        else
+            throw std::runtime_error(
+                "Unrecognized input argument for friction mode"
+            );  
+    }
+
     // Define required input parameters
     const T R = static_cast<T>(json_data["R"].as_double());
     const T Rcell = static_cast<T>(json_data["Rcell"].as_double());
@@ -42,7 +60,6 @@ int main(int argc, char** argv)
     const T E0 = static_cast<T>(json_data["E0"].as_double());
     const T Ecell = static_cast<T>(json_data["Ecell"].as_double()); 
     const T eta_ambient = static_cast<T>(json_data["eta_ambient"].as_double()); 
-    const T eta_surface = static_cast<T>(json_data["eta_surface"].as_double());
     const T max_stepsize = static_cast<T>(json_data["max_stepsize"].as_double());
     const T min_stepsize = static_cast<T>(json_data["min_stepsize"].as_double()); 
     const T dt_write = static_cast<T>(json_data["dt_write"].as_double()); 
@@ -54,10 +71,36 @@ int main(int argc, char** argv)
     const int n_cells = json_data["n_cells"].as_int64();
     const T growth_mean = static_cast<T>(json_data["growth_mean"].as_double());
     const T growth_std = static_cast<T>(json_data["growth_std"].as_double());
-    const T sigma0_mean1 = static_cast<T>(json_data["sigma0_mean1"].as_double()); 
-    const T sigma0_std1 = static_cast<T>(json_data["sigma0_std1"].as_double()); 
-    const T sigma0_mean2 = static_cast<T>(json_data["sigma0_mean2"].as_double());
-    const T sigma0_std2 = static_cast<T>(json_data["sigma0_std2"].as_double());
+    T sigma0_mean1, sigma0_std1, sigma0_mean2, sigma0_std2; 
+    T eta1_mean1, eta1_std1, eta1_mean2, eta1_std2;
+    if (friction_mode == 0 || friction_mode == 2)
+    { 
+        sigma0_mean1 = static_cast<T>(json_data["sigma0_mean1"].as_double()); 
+        sigma0_std1 = static_cast<T>(json_data["sigma0_std1"].as_double()); 
+        sigma0_mean2 = static_cast<T>(json_data["sigma0_mean2"].as_double());
+        sigma0_std2 = static_cast<T>(json_data["sigma0_std2"].as_double());
+    }
+    else 
+    {
+        sigma0_mean1 = static_cast<T>(json_data["sigma0"].as_double());
+        sigma0_std1 = 0.0; 
+        sigma0_mean2 = 0.0; 
+        sigma0_std2 = 0.0; 
+    }
+    if (friction_mode == 1 || friction_mode == 2)
+    {
+        eta1_mean1 = static_cast<T>(json_data["eta1_mean1"].as_double()); 
+        eta1_std1 = static_cast<T>(json_data["eta1_std1"].as_double()); 
+        eta1_mean2 = static_cast<T>(json_data["eta1_mean2"].as_double());
+        eta1_std2 = static_cast<T>(json_data["eta1_std2"].as_double());
+    }
+    else 
+    {
+        eta1_mean1 = static_cast<T>(json_data["eta_surface"].as_double());
+        eta1_std1 = 0.0; 
+        eta1_mean2 = 0.0; 
+        eta1_std2 = 0.0; 
+    }
     const T lifetime_mean1 = static_cast<T>(json_data["lifetime_mean1"].as_double()); 
     const T lifetime_mean2 = static_cast<T>(json_data["lifetime_mean2"].as_double()); 
     const T daughter_length_std = static_cast<T>(json_data["daughter_length_std"].as_double());
@@ -88,12 +131,37 @@ int main(int argc, char** argv)
     growth_means << growth_mean, growth_mean; 
     growth_stds << growth_std, growth_std; 
 
-    // Vectors of friction coefficient means and standard deviations
-    std::vector<int> group_attributes { __colidx_sigma0 };
-    Array<T, Dynamic, Dynamic> attribute_means(2, 1);
-    Array<T, Dynamic, Dynamic> attribute_stds(2, 1);
-    attribute_means << sigma0_mean1, sigma0_mean2;
-    attribute_stds << sigma0_std1, sigma0_std2;
+    // Vectors of cell-surface adhesion energy density and/or friction
+    // coefficient means and standard deviations
+    std::vector<int> group_attributes; 
+    Array<T, Dynamic, Dynamic> attribute_means, attribute_stds;
+    if (friction_mode == 0)
+    {
+        group_attributes.push_back(__colidx_sigma0);
+        attribute_means.resize(2, 1); 
+        attribute_stds.resize(2, 1);  
+        attribute_means << sigma0_mean1, sigma0_mean2;
+        attribute_stds << sigma0_std1, sigma0_std2;
+    } 
+    else if (friction_mode == 1)
+    {
+        group_attributes.push_back(__colidx_maxeta1);
+        attribute_means.resize(2, 1); 
+        attribute_stds.resize(2, 1);  
+        attribute_means << eta1_mean1, eta1_mean2;
+        attribute_stds << eta1_std1, eta1_std2;
+    }
+    else 
+    {
+        group_attributes.push_back(__colidx_maxeta1);
+        group_attributes.push_back(__colidx_sigma0); 
+        attribute_means.resize(2, 2); 
+        attribute_stds.resize(2, 2);  
+        attribute_means << eta1_mean1, sigma0_mean1,
+                           eta1_mean2, sigma0_mean2;
+        attribute_stds << eta1_std1, sigma0_std1,
+                          eta1_std2, sigma0_std2;
+    }
 
     // Switching rates between groups 1 and 2
     Array<T, Dynamic, Dynamic> switch_rates(2, 2); 
@@ -113,7 +181,7 @@ int main(int argc, char** argv)
     // coefficients
     Array<T, Dynamic, Dynamic> cells(1, __ncols_required);
     cells << 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, L0, L0 / 2, 0, growth_mean,
-             eta_ambient, eta_surface, eta_surface, sigma0_mean1, 1;
+             eta_ambient, eta1_mean1, eta1_mean1, sigma0_mean1, 1;
 
     // Initialize parent IDs 
     std::vector<int> parents; 
