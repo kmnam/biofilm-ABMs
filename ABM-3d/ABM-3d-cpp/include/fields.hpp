b166603334ed86dc1fd5992283718a5d7d63068b
Kee Myoung Nam <kmnam.99.12@gmail.com>
2025-06-30 18:03:37 -0400

    More operators

--- ABM-3d/ABM-3d-cpp/include/fields.hpp
+++ ABM-3d/ABM-3d-cpp/include/fields.hpp
@@ -44,54 +44,191 @@ template <int p>
 class Fp
 {
     public:
-        boost::multiprecision::gmp_rational value;
+        boost::multiprecision::mpq_rational value;
 
         Fp(const int x)
         {
             if (p == 0)
-                this->value = static_cast<boost::multiprecision::gmp_rational>(x);
+                this->value = static_cast<boost::multiprecision::mpq_rational>(x);
             else 
-                this->value = static_cast<boost::multiprecision::gmp_rational>(x % p);  
+                this->value = static_cast<boost::multiprecision::mpq_rational>(x % p);  
+        }
+
+        Fp(const boost::multiprecision::mpq_rational x)
+        {
+            if (p == 0)
+                this->value = x; 
+            else if (boost::multiprecision::denominator(x) == 1)
+                this->value = x; 
+            else    // x is not an integer and p is nonzero 
+                throw std::runtime_error(
+                    "Cannot assign non-integer value in field of finite "
+                    "characteristic"
+                );  
         }
 
         ~Fp()
         {
         }
 
-        Fp<p> operator+(const Fp<p>& x, const Fp<p>& y)
+        // ---------------------------------------------------------- //
+        //              EQUALITY AND INEQUALITY OPERATORS             //
+        // ---------------------------------------------------------- //
+        bool operator==(const int& x) const 
         {
-            return Fp<p>(x.value + y.value); 
+            if (p == 0)
+                return (this->value == x); 
+            else 
+                return ((this->value - x) % p == 0); 
+        }
+
+        bool operator==(const boost::multiprecision::mpq_rational& x) const
+        {
+            if (p == 0)
+                return (this->value == x); 
+            else 
+                return ((this->value - x) % p == 0); 
+        }
+
+        bool operator==(const Fp<p>& x) const
+        {
+            if (p == 0)
+                return (this->value == x.value); 
+            else 
+                return ((this->value - x.value) % p == 0); 
+        }
+
+        bool operator!=(const int& x) const 
+        {
+            return !(*this == x); 
+        }
+
+        bool operator!=(const boost::multiprecision::mpq_rational& x) const 
+        {
+            return !(*this == x); 
+        }
+
+        bool operator!=(const Fp<p>& x) const 
+        {
+            return !(*this == x); 
+        }
+
+        // ---------------------------------------------------------- //
+        //                  INCREMENT AND DECREMENT                   //
+        // ---------------------------------------------------------- //
+        Fp<p>& operator++()
+        {
+            this->value++; 
+            if (p != 0 && this->value == p)
+                this->value = 0; 
+            return *this; 
+        }
+        
+        Fp<p>& operator--()
+        {
+            this->value--;
+            if (p != 0 && this->value == -1)
+                this->value = p - 1;
+            return *this;  
+        }
+
+        // ---------------------------------------------------------- //
+        //                    ARITHMETIC OPERATORS                    //
+        // ---------------------------------------------------------- //
+        Fp<p>& operator+=(const int& y)
+        {
+            this->value += y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
+        }
+
+        Fp<p>& operator+=(const boost::multiprecision::mpq_rational& y)
+        {
+            #ifdef CHECK_FP_RATIONAL_NONINTEGER
+                if (p != 0 && boost::multiprecision::denominator(y) != 1)
+                    throw std::runtime_error(
+                        "Cannot add non-integer number to number in field of "
+                        "finite characteristic"
+                    ); 
+            #endif
+            this->value += y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
         }
 
         Fp<p>& operator+=(const Fp<p>& y)
         {
             this->value += y.value;
-            this->value %= p;  
+            if (p != 0)
+                this->value %= p;
+            return *this;  
         }
 
-        Fp<p> operator-(const Fp<p>& x, const Fp<p>& y)
+        Fp<p>& operator-=(const int& y)
         {
-            return Fp<p>(x.value - y.value); 
+            this->value -= y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
+        }
+
+        Fp<p>& operator-=(const boost::multiprecision::mpq_rational& y)
+        {
+            #ifdef CHECK_FP_RATIONAL_NONINTEGER
+                if (p != 0 && boost::multiprecision::denominator(y) != 1)
+                    throw std::runtime_error(
+                        "Cannot subtract non-integer number from number in "
+                        "field of finite characteristic"
+                    ); 
+            #endif
+            this->value -= y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
         }
 
         Fp<p>& operator-=(const Fp<p>& y)
         {
             this->value -= y.value;
-            this->value %= p;  
+            if (p != 0)
+                this->value %= p;  
+            return *this; 
         }
 
-        Fp<p> operator*(const Fp<p>& x, const Fp<p>& y)
+        Fp<p>& operator*=(const int& y)
         {
-            return Fp<p>(x.value * y.value); 
+            this->value *= y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
+        }
+
+        Fp<p>& operator*=(const boost::multiprecision::mpq_rational& y)
+        {
+            #ifdef CHECK_FP_RATIONAL_NONINTEGER
+                if (p != 0 && boost::multiprecision::denominator(y) != 1)
+                    throw std::runtime_error(
+                        "Cannot subtract non-integer number from number in "
+                        "field of finite characteristic"
+                    ); 
+            #endif
+            this->value *= y; 
+            if (p != 0)
+                this->value %= p; 
+            return *this; 
         }
 
         Fp<p>& operator*=(const Fp<p>& y)
         {
             this->value *= y.value;
-            this->value %= p;  
+            if (p != 0)
+                this->value %= p;
+            return *this;  
         }
 
-        Fp<p> operator/(const Fp<p>& x, const Fp<p>& y)
+        Fp<p>& operator/=(const int& y)
         {
             // Check that y is nonzero 
             if (y == 0)
@@ -99,11 +236,44 @@ class Fp
 
             // If p == 0, then simply return x / y
             if (p == 0)
-                return Fp<p>(x.value / y.value);  
+            {
+                this->value /= y;
+            }
+            else    // Otherwise, multiply x by the multiplicative inverse of y
+            {
+                int t = getMultiplicativeInverse<p>(y); 
+                this->value *= t;
+                this->value %= p;
+            }
+            return *this;  
+        }
 
-            // Otherwise, multiply x by the multiplicative inverse of y 
-            int t = getMultiplicativeInverse<p>(y.value); 
-            return Fp<p>(x_ * t);  
+        Fp<p>& operator/=(const boost::multiprecision::mpq_rational& y)
+        {
+            #ifdef CHECK_FP_RATIONAL_NONINTEGER
+                if (p != 0 && boost::multiprecision::denominator(y) != 1)
+                    throw std::runtime_error(
+                        "Cannot subtract non-integer number from number in "
+                        "field of finite characteristic"
+                    ); 
+            #endif
+            
+            // Check that y is nonzero
+            if (y == 0)
+                throw std::runtime_error("Cannot divide by zero");
+
+            // If p == 0, then simply return x / y
+            if (p == 0)
+            {
+                this->value /= y;
+            }
+            else    // Otherwise, multiply x by the multiplicative inverse of y
+            {
+                int t = getMultiplicativeInverse<p>(boost::multiprecision::numerator(y)); 
+                this->value *= t;
+                this->value %= p;
+            }
+            return *this;  
         }
 
         Fp<p>& operator/=(const Fp<p>& y)
@@ -114,17 +284,51 @@ class Fp
 
             // If p == 0, then simply return x / y
             if (p == 0)
+            {
                 this->value /= y.value;
-
-            // Otherwise, multiply x by the multiplicative inverse of y 
-            int t = getMultiplicativeInverse<p>(y.value); 
-            this->value *= t;
-            this->value %= p; 
+            }
+            else    // Otherwise, multiply x by the multiplicative inverse of y
+            {
+                int t = getMultiplicativeInverse<p>(y.value); 
+                this->value *= t;
+                this->value %= p;
+            }
+            return *this;  
         } 
+};
+
+Fp<p> operator+(const Fp<p>& x, const Fp<p>& y) const 
+{
+    return Fp<p>(x.value + y.value); 
+}
+
+Fp<p> operator-(const Fp<p>& x, const Fp<p>& y) const
+{
+    return Fp<p>(x.value - y.value); 
+}
+
+Fp<p> operator*(const Fp<p>& x, const Fp<p>& y)
+{
+    return Fp<p>(x.value * y.value); 
+}
+
+Fp<p> operator/(const Fp<p>& x, const Fp<p>& y)
+{
+    // Check that y is nonzero 
+    if (y == 0)
+        throw std::runtime_error("Cannot divide by zero");
+
+    // If p == 0, then simply return x / y
+    if (p == 0)
+        return Fp<p>(x.value / y.value);  
+
+    // Otherwise, multiply x by the multiplicative inverse of y 
+    int t = getMultiplicativeInverse<p>(y.value); 
+    return Fp<p>(x_ * t);  
 }
 
 template <>
-Fp<0>::Fp(const boost::multiprecision::gmp_rational x)
+Fp<0>::Fp(const boost::multiprecision::mpq_rational x)
 {
     this->value = x; 
 }
