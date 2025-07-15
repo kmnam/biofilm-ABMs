53532bb4bcd65c5e718831cab68e87975a9a539a
Kee Myoung Nam <kmnam.99.12@gmail.com>
2025-07-15 16:59:34 -0400

    Removed graph functions

--- ABM-3d/ABM-3d-cpp/include/topology.hpp
+++ ABM-3d/ABM-3d-cpp/include/topology.hpp
@@ -1,11 +1,11 @@
 /**
- * Classes and functions for graphs and simplicial complexes. 
+ * Classes and functions for tries and simplicial complexes. 
  *
  * Author:
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     7/13/2025
+ *     7/15/2025
  */
 
 #ifndef SIMPLICIAL_COMPLEXES_3D_HPP
@@ -13,11 +13,17 @@
 
 #include <iostream>
 #include <memory>
+#include <utility>
+#include <vector>
 #include <stack>
 #include <queue>
+#include <map>
+#include <unordered_map>
+#include <functional>
 #include <Eigen/Dense>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/connected_components.hpp>
+#include <boost/container_hash/hash.hpp>
 #include <CGAL/Gmpz.h>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
@@ -33,339 +39,6 @@ typedef boost::adjacency_list<boost::hash_setS, boost::vecS, boost::undirectedS,
 typedef CGAL::Gmpz ET; 
 typedef CGAL::Quadratic_program<int> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution; 
-
-/** ------------------------------------------------------------------- //
- *                            GRAPH UTILITIES                           //
- *  ------------------------------------------------------------------- */
-/**
- * Define and return a graph in which each cell is a vertex, and two vertices
- * are connected by an edge if (1) both cells are in group 1 and (2) the two
- * cells are within 2 * R distance of each other.
- *
- * @param cells Input population of cells.  
- * @param R Cell radius, including the EPS. 
- * @param Ldiv Cell division length. 
- * @returns Cell-cell neighbor graph. 
- */
-template <typename T>
-Graph getNeighborGraph(const Ref<const Array<T, Dynamic, Dynamic> >& cells, 
-                       const T R, const T Ldiv)
-{
-    // Get the cell-cell distance between each pair of neighboring cells in
-    // the population
-    Array<T, Dynamic, 7> neighbors = getCellNeighbors<T>(cells, 2 * R, R, Ldiv);
-
-    // Define a graph and add a vertex for each cell  
-    Graph graph(cells.rows());
-
-    // Add an edge between each pair of group 1 cells that are within 2 * R
-    // distance 
-    for (int k = 0; k < neighbors.rows(); ++k)
-    {
-        int i = neighbors(k, 0); 
-        int j = neighbors(k, 1); 
-        T dij = neighbors(k, Eigen::seq(2, 4)).matrix().norm();
-        if (dij < 2 * R && cells(i, __colidx_group) == 1 && cells(j, __colidx_group) == 1)
-            boost::add_edge(i, j, EdgeProperty(dij), graph);
-    }
-
-    return graph; 
-}
-
-/**
- * Get the connected components in the given graph.
- *
- * The i-th entry in the returned std::vector is the index of the connected
- * component containing vertex i.
- *
- * @param graph Input graph. 
- * @returns Indices of connected components containing each vertex.  
- */
-template <typename T>
-std::vector<int> getConnectedComponents(const Graph& graph)
-{
-    std::vector<int> components(boost::num_vertices(graph));
-    boost::connected_components(graph, &components[0]);
-
-    return components;  
-}
-
-/**
- * Get the degrees of all vertices in the given graph. 
- *
- * The i-th entry in the returned array is the number of edges incident on 
- * the vertex i. 
- *
- * @param graph Input graph. 
- * @returns Degrees of vertices in the graph. 
- */
-template <typename T>
-Array<int, Dynamic, 1> getDegrees(const Graph& graph)
-{
-    Array<int, Dynamic, 1> degrees = Array<int, Dynamic, 1>::Zero(boost::num_vertices(graph));
-    std::pair<boost::graph_traits<Graph>::edge_iterator, 
-              boost::graph_traits<Graph>::edge_iterator> it; 
-    for (it = boost::edges(graph); it.first != it.second; ++it.first)
-    {
-        boost::graph_traits<Graph>::edge_descriptor edge = *(it.first);
-        int i = boost::source(edge, graph); 
-        int j = boost::target(edge, graph);
-        degrees(i) += 1; 
-        degrees(j) += 1; 
-    }
-
-    return degrees; 
-}
-
-/**
- * Get the local clustering coefficients for all vertices in the given graph. 
- *
- * @param graph Input graph. 
- * @returns Local clustering coefficients for all vertices in the graph. 
- */
-template <typename T>
-Array<T, Dynamic, 1> getLocalClusteringCoefficients(const Graph& graph)
-{
-    Array<T, Dynamic, 1> coefs = Array<T, Dynamic, 1>::Zero(boost::num_vertices(graph));
-    std::pair<boost::graph_traits<Graph>::out_edge_iterator, 
-              boost::graph_traits<Graph>::out_edge_iterator> it;
-    for (int i = 0; i < boost::num_vertices(graph); ++i)
-    {
-        // First identify the neighbors of i
-        std::vector<int> neighbors;  
-        for (it = boost::out_edges(i, graph); it.first != it.second; ++it.first)
-        {
-            // Rigorously check which vertex is the neighbor and which is i
-            // (the former should always be the target) 
-            boost::graph_traits<Graph>::edge_descriptor edge = *(it.first);
-            int j = boost::source(edge, graph); 
-            int k = boost::target(edge, graph);
-            if (i == j) 
-                neighbors.push_back(k); 
-            else 
-                neighbors.push_back(j); 
-        }
-        int n_neighbors = neighbors.size();
-
-        // If degree = 0 or 1, then the local clustering coefficient is undefined
-        if (n_neighbors == 0 || n_neighbors == 1)
-        {
-            coefs(i) = -1;
-        }
-        // Otherwise, check, for each pair of neighbors, if they are connected
-        // by a third edge
-        else
-        {
-            T n_cluster = 0.0; 
-            for (int j = 0; j < neighbors.size(); ++j)
-            {
-                for (int k = j + 1; k < neighbors.size(); ++k)
-                {
-                    if (boost::edge(neighbors[j], neighbors[k], graph).second)
-                        n_cluster += 1; 
-                }
-            } 
-            coefs(i) = 2 * n_cluster / (n_neighbors * (n_neighbors - 1));
-        }    
-    }
-
-    return coefs;
-}
-
-/**
- * Get all triangles (3-cliques) in the given graph. 
- *
- * Each row in the returned array is a combination of three vertices that 
- * form a triangle. 
- *
- * @param graph Input graph. 
- * @returns Array of triangle-forming vertices. 
- */
-template <typename T>
-Array<int, Dynamic, 3> getTriangles(const Graph& graph)
-{
-    const int n = boost::num_vertices(graph);
-    int nt = 0;  
-    Array<int, Dynamic, 3> triangles(nt, 3);
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = i + 1; j < n; ++j)
-        {
-            if (boost::edge(i, j, graph).second)
-            {
-                for (int k = j + 1; k < n; ++k)
-                {
-                    if (boost::edge(j, k, graph).second && boost::edge(i, k, graph).second)
-                    {
-                        nt++; 
-                        triangles.conservativeResize(nt, 3); 
-                        triangles(nt - 1, 0) = i; 
-                        triangles(nt - 1, 1) = j; 
-                        triangles(nt - 1, 2) = k; 
-                    }
-                }
-            }
-        }
-    }
-
-    return triangles; 
-}
-
-/**
- * Get all tetrahedra (4-cliques) in the given graph. 
- *
- * Each row in the returned array is a combination of four vertices that 
- * form a tetrahedron.  
- *
- * @param graph Input graph. 
- * @returns Array of tetrahedron-forming vertices. 
- */
-template <typename T>
-Array<int, Dynamic, 4> getTetrahedra(const Graph& graph)
-{
-    const int n = boost::num_vertices(graph);
-    int nt = 0;  
-    Array<int, Dynamic, 4> tetrahedra(nt, 4);
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = i + 1; j < n; ++j)
-        {
-            if (boost::edge(i, j, graph).second)    // Check that i-j
-            {
-                for (int k = j + 1; k < n; ++k)
-                {
-                    // Check that j-k and i-k
-                    if (boost::edge(j, k, graph).second && boost::edge(i, k, graph).second)
-                    {
-                        for (int m = k + 1; m < n; ++m)
-                        {
-                            // Check that i-m, j-m, and k-m
-                            if (boost::edge(i, m, graph).second && 
-                                boost::edge(j, m, graph).second &&
-                                boost::edge(k, m, graph).second)
-                            {
-                                nt++; 
-                                tetrahedra.conservativeResize(nt, 4); 
-                                tetrahedra(nt - 1, 0) = i; 
-                                tetrahedra(nt - 1, 1) = j; 
-                                tetrahedra(nt - 1, 2) = k;
-                                tetrahedra(nt - 1, 3) = m;
-                            }
-                        } 
-                    }
-                }
-            }
-        }
-    }
-
-    return tetrahedra; 
-}
-
-/**
- * Write the given graph and its connectivity information to file. 
- *
- * @param graph Input graph. 
- * @param components Vector of connected component sizes, from largest to
- *                   smallest.
- * @param degrees Vector of vertex degrees. 
- * @param filename Path to output file. 
- * @param write_cluster_coefs If true, write local clustering coefficients
- *                            to file. 
- * @param cluster_coefs Vector of local clustering coefficients. 
- * @param write_triangles If true, write triangles to file. 
- * @param triangles Array of vertices that form triangles. 
- * @param write_tetrahedra If true, write tetrahedra to file. 
- * @param tetrahedra Array of vertices that form tetrahedra. 
- */
-template <typename T>
-void writeGraph(const Graph& graph, std::vector<int>& components,
-                const Ref<const Array<int, Dynamic, 1> >& degrees,
-                const std::string filename, const bool write_cluster_coefs,  
-                const Ref<const Array<T, Dynamic, 1> >& cluster_coefs,         
-                const bool write_triangles, 
-                const Ref<const Array<int, Dynamic, 3> >& triangles,
-                const bool write_tetrahedra,
-                const Ref<const Array<int, Dynamic, 4> >& tetrahedra)
-{
-    // Get the component sizes 
-    int num_components = *std::max_element(components.begin(), components.end()) + 1;
-    Array<int, Dynamic, 1> component_sizes = Array<int, Dynamic, 1>::Zero(num_components); 
-    for (int i = 0; i < components.size(); ++i)
-        component_sizes(components[i]) += 1;
-
-    // Open output file and write header 
-    std::ofstream outfile(filename);
-    outfile << std::setprecision(10); 
-    outfile << "NUM_VERTICES\t" << boost::num_vertices(graph) << std::endl; 
-    outfile << "NUM_EDGES\t" << boost::num_edges(graph) << std::endl; 
-    outfile << "NUM_COMPONENTS\t" << num_components << std::endl;
-    if (write_triangles)
-        outfile << "NUM_TRIANGLES\t" << triangles.rows() << std::endl;
-    if (write_tetrahedra)
-        outfile << "NUM_TETRAHEDRA\t" << tetrahedra.rows() << std::endl;
-
-    // Compute the degree distribution
-    Array<int, Dynamic, 1> degree_dist = Array<int, Dynamic, 1>::Zero(degrees.maxCoeff() + 1);
-    for (int i = 0; i < boost::num_vertices(graph); ++i)
-        degree_dist(degrees(i)) += 1;
-    for (int i = 0; i <= degrees.maxCoeff(); ++i) 
-        outfile << "DEGREE_DIST\t" << i << '\t' << degree_dist(i) << std::endl; 
-
-    // Write the vertices to the output file 
-    for (int i = 0; i < boost::num_vertices(graph); ++i)
-    {
-        if (write_cluster_coefs)
-            outfile << "VERTEX\t" << i << "\tCOMPONENT:" << components[i]
-                                       << "\tDEGREE:" << degrees(i)
-                                       << "\tCLUSTER:" << cluster_coefs(i) << std::endl;
-        else 
-            outfile << "VERTEX\t" << i << "\tCOMPONENT:" << components[i]
-                                       << "\tDEGREE:" << degrees(i) << std::endl;
-    }        
-
-    // Write the edges to the output file
-    std::pair<boost::graph_traits<Graph>::edge_iterator, 
-              boost::graph_traits<Graph>::edge_iterator> it;
-    auto weights = boost::get(boost::edge_weight, graph);  
-    for (it = boost::edges(graph); it.first != it.second; ++it.first)
-    {
-        boost::graph_traits<Graph>::edge_descriptor edge = *(it.first);
-        int i = boost::source(edge, graph); 
-        int j = boost::target(edge, graph);
-        double dij = weights[edge];
-        outfile << "EDGE\t" << i << '\t' << j << '\t' << dij << std::endl; 
-    }
-
-    // Write the component sizes to the output file 
-    for (int i = 0; i < num_components; ++i)
-        outfile << "COMPONENT\t" << i << '\t' << component_sizes(i) << std::endl;
-
-    // Write triangles to the output file, if desired 
-    if (write_triangles)
-    {
-        for (int i = 0; i < triangles.rows(); ++i)
-        {
-            outfile << "TRIANGLE\t" << triangles(i, 0) << '\t'
-                                    << triangles(i, 1) << '\t'
-                                    << triangles(i, 2) << std::endl;
-        } 
-    }
-    
-    // Write tetrahedra to the output file, if desired 
-    if (write_tetrahedra)
-    {
-        for (int i = 0; i < tetrahedra.rows(); ++i)
-        {
-            outfile << "TETRAHEDRA\t" << tetrahedra(i, 0) << '\t'
-                                      << tetrahedra(i, 1) << '\t'
-                                      << tetrahedra(i, 2) << '\t'
-                                      << tetrahedra(i, 3) << std::endl; 
-        }
-    }
-
-    // Close output file 
-    outfile.close();
-}
 
 /** ------------------------------------------------------------------- //
  *                                 TRIES                                //
@@ -830,7 +503,8 @@ class SimplicialComplex3D
     private:
         Array<T, Dynamic, 3> points;     // Array of point coordinates
         int dim;                         // Dimension
-        Trie tree;                       // Simplex tree  
+        Trie tree;                       // Simplex tree
+        Graph one_skeleton;              // 1-skeleton as a Boost::Graph object  
 
     public:
         /**
@@ -857,7 +531,13 @@ class SimplicialComplex3D
             for (int i = 0; i < triangles.rows(); ++i)
                 this->tree.template insert<3>(triangles.row(i).transpose());
             for (int i = 0; i < tetrahedra.rows(); ++i)
-                this->tree.template insert<4>(tetrahedra.row(i).transpose()); 
+                this->tree.template insert<4>(tetrahedra.row(i).transpose());
+
+            // Populate the 1-skeleton (with uniform weights) 
+            for (int i = 0; i < points.rows(); ++i)
+                boost::add_vertex(this->one_skeleton); 
+            for (int i = 0; i < edges.rows(); ++i)
+                boost::add_edge(edges(i, 0), edges(i, 1), EdgeProperty(1.0), this->one_skeleton); 
         }
 
         /**
@@ -873,6 +553,8 @@ class SimplicialComplex3D
                             Graph& graph)
         {
             this->points = points;
+            for (int i = 0; i < points.rows(); ++i)
+                boost::add_vertex(this->one_skeleton); 
 
             // Construct the simplex tree, starting with the 0-simplices 
             for (int i = 0; i < points.rows(); ++i)
@@ -887,7 +569,8 @@ class SimplicialComplex3D
                 int j = boost::source(edge, graph); 
                 int k = boost::target(edge, graph);
                 std::vector<int> edge_vec { j, k }; 
-                this->tree.insert(edge_vec); 
+                this->tree.insert(edge_vec);
+                boost::add_edge(j, k, EdgeProperty(1.0), this->one_skeleton); 
             }
 
             // Define the triangles and tetrahedra from the 3- and 4-cliques 
@@ -913,7 +596,18 @@ class SimplicialComplex3D
                             Trie& tree)
         {
             this->points = points;
-            this->tree = tree; 
+            this->tree = tree;
+
+            // Populate the 1-skeleton (with uniform weights) 
+            for (int i = 0; i < points.rows(); ++i)
+                boost::add_vertex(this->one_skeleton); 
+            std::vector<std::vector<int> > edges = this->tree.getSubstrings(true, 2); 
+            for (const auto& edge : edges)
+            {
+                int j = edge[0]; 
+                int k = edge[1]; 
+                boost::add_edge(j, k, EdgeProperty(1.0), this->one_skeleton);
+            } 
         }
 
         /**
@@ -978,7 +672,11 @@ class SimplicialComplex3D
             // If dim == -1, then count the total number of simplices 
             if (dim == -1)
                 return this->tree.getSubstrings(true).size();
-            else 
+            else if (dim == 0)
+                return this->points.rows(); 
+            else if (dim == 1)
+                return boost::num_edges(this->one_skeleton);
+            else    // dim > 1 
                 return this->tree.getSubstrings(true, dim + 1).size();  
         }
 
@@ -1039,7 +737,27 @@ class SimplicialComplex3D
             for (int i = 0; i < triangles.rows(); ++i)
                 this->tree.template insert<3>(triangles.row(i).transpose()); 
             for (int i = 0; i < tetrahedra.rows(); ++i)
-                this->tree.template insert<4>(tetrahedra.row(i).transpose()); 
+                this->tree.template insert<4>(tetrahedra.row(i).transpose());
+
+            // Populate the 1-skeleton (with uniform weights)
+            // TODO Clear graph  
+            for (int i = 0; i < points.rows(); ++i)
+                boost::add_vertex(this->one_skeleton); 
+            for (int i = 0; i < edges.rows(); ++i)
+                boost::add_edge(edges(i, 0), edges(i, 1), EdgeProperty(1.0), this->one_skeleton); 
+        }
+
+        /**
+         * Return the shortest path from vertex u to vertex v in the
+         * simplicial complex. 
+         *
+         * @param u First vertex. 
+         * @param v Second vertex. 
+         * @returns Shortest path from u to v in the simplicial complex. 
+         */
+        std::vector<int> getMinimumWeightPath(const int u, const int v) const
+        {
+            return ::getMinimumWeightPath(this->one_skeleton, u, v); 
         }
 
         /**
