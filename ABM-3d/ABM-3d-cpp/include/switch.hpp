a725a8eca7311e056502e631c06c8ba13bbdc0a1
Kee-Myoung Nam <kmnam.99.12@gmail.com>
2025-08-10 19:29:12 -0400

    Simplified switching

--- ABM-3d/ABM-3d-cpp/include/switch.hpp
+++ ABM-3d/ABM-3d-cpp/include/switch.hpp
@@ -11,7 +11,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     2/26/2025
+ *     8/10/2025
  */
 
 #ifndef BIOFILM_SWITCH_3D_HPP
@@ -34,30 +34,24 @@ enum class SwitchMode
  * Randomly switch cells from one group to another.
  *
  * @param cells Existing population of cells.
- * @param group_attributes Indices of attributes that differ between groups. 
  * @param n_groups Number of distinct groups.
  * @param dt Timestep. 
  * @param switch_rates Matrix of switching rates between groups. 
  * @param growth_dists Vector of growth rate distributions, one for each
  *                     group.
- * @param attribute_dists Dictionary of attribute distributions, one for each
- *                        group-attribute pair.
  * @param rng Random number generator. 
  * @param uniform_dist Pre-defined uniform distribution between 0 and 1.
  */
 template <typename T>
 void switchGroupsMarkov(Ref<Array<T, Dynamic, Dynamic> > cells,
-                        std::vector<int>& group_attributes,
                         const int n_groups, const T dt,
                         const Ref<const Array<T, Dynamic, Dynamic> >& switch_rates,
                         std::vector<std::function<T(boost::random::mt19937&)> >& growth_dists,
-                        std::map<std::pair<int, int>, std::function<T(boost::random::mt19937&)> >& attribute_dists,
                         boost::random::mt19937& rng,
                         boost::random::uniform_01<>& uniform_dist)
 {
     // First identify cells to switch groups within the given timestep
     Array<T, Dynamic, Dynamic> switch_probs = dt * switch_rates;
-    const int n_attributes = group_attributes.size(); 
     for (int i = 0; i < cells.rows(); ++i)
     {
         // Which group is the cell currently in? 
@@ -74,16 +68,10 @@ void switchGroupsMarkov(Ref<Array<T, Dynamic, Dynamic> > cells,
                 if (r > total && r < total + switch_prob)
                 {
                     // If the cell is to switch to group j, sample the cell's
-                    // new growth rate and attribute values 
+                    // new growth rate
                     cells(i, __colidx_group) = j + 1;
                     T growth_rate = growth_dists[j](rng);
                     cells(i, __colidx_growth) = growth_rate;
-                    for (int k = 0; k < n_attributes; ++k)
-                    {
-                        auto pair = std::make_pair(j, k);
-                        T attribute = attribute_dists[pair](rng);
-                        cells(i, group_attributes[k]) = attribute; 
-                    }
                     break;
                 }
                 total += switch_prob;
@@ -99,29 +87,22 @@ void switchGroupsMarkov(Ref<Array<T, Dynamic, Dynamic> > cells,
  * @param cells Existing population of cells.
  * @param daughter_pairs Indices of pairs of daughter cells that have just 
  *                       divided. 
- * @param group_attributes Indices of attributes that differ between groups.
  * @param n_groups Number of distinct groups.
  * @param switch_probs Matrix of switching probabilities between groups. 
  * @param growth_dists Vector of growth rate distributions, one for each
  *                     group.
- * @param attribute_dists Dictionary of attribute distributions, one for each
- *                        group-attribute pair.
  * @param rng Random number generator.
  * @param uniform_dist Pre-defined uniform distribution between 0 and 1.
  */
 template <typename T>
 void switchGroupsInherit(Ref<Array<T, Dynamic, Dynamic> > cells,
                          std::vector<std::pair<int, int> >& daughter_pairs,
-	                 std::vector<int>& group_attributes,
 		         const int n_groups, 
 		         const Ref<const Array<T, Dynamic, Dynamic> >& switch_probs,
 		         std::vector<std::function<T(boost::random::mt19937&)> >& growth_dists,
-		         std::map<std::pair<int, int>, std::function<T(boost::random::mt19937&)> >& attribute_dists,
 		         boost::random::mt19937& rng,
 		         boost::random::uniform_01<>& uniform_dist)
 {
-    const int n_attributes = group_attributes.size();
-
     // For each pair of daughter cells ... 
     for (const std::pair<int, int>& pair : daughter_pairs)
     {
@@ -147,17 +128,10 @@ void switchGroupsInherit(Ref<Array<T, Dynamic, Dynamic> > cells,
 			// first choose the daughter cell to switch ...
 			T s = uniform_dist(rng); 
 			int idx_switch = (s < 0.5 ? i : j); 
-			// ... then sample the cell's new growth rate and attribute
-			// values 
+			// ... then sample the cell's new growth rate 
 			cells(idx_switch, __colidx_group) = k + 1;
 			T growth_rate = growth_dists[k](rng);
 			cells(idx_switch, __colidx_growth) = growth_rate;
-			for (int m = 0; m < n_attributes; ++m)
-			{
-			    auto pair = std::make_pair(k, m);
-			    T attribute = attribute_dists[pair](rng);
-			    cells(idx_switch, group_attributes[m]) = attribute;
-			}
 			break;
 		    }
 		    total += switch_prob;
